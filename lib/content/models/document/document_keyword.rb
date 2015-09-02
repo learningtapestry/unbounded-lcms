f3478853feefa2e5c3c63d200b951dfa6b13ac79
RM Saksida <rm@saksida.com.br>
2015-09-01 22:53:34 -0300

    Remove unused models

--- lib/content/models/document/document_keyword.rb
+++
@@ -1,8 +0,0 @@
-module Content
-  module Models
-    class DocumentKeyword < ActiveRecord::Base
-      belongs_to :document
-      belongs_to :keyword
-    end
-  end
-end
--- lib/content/models/lobject/lobject_keyword.rb
+++
@@ -1,9 +0,0 @@
-module Content
-  module Models
-    class LobjectKeyword < ActiveRecord::Base
-      belongs_to :lobject
-      belongs_to :document
-      belongs_to :keyword
-    end
-  end
-end
