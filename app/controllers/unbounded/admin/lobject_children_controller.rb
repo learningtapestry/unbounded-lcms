a44534a11be70b1eba30e485554e5597f745f2d1
Alex <nizamov.alex@gmail.com>
2015-09-09 18:56:46 +0500

    Add tree nodes via remote form

--- app/controllers/unbounded/admin/lobject_children_controller.rb
+++ app/controllers/unbounded/admin/lobject_children_controller.rb
@@ -4,7 +4,11 @@ module Unbounded
       def create
         child_params = params.require('content_models_lobject_child').permit(:child_id, :lobject_collection_id, :parent_id, :position)
         @lobject_child = LobjectChild.create(child_params)
-        redirect_to edit_unbounded_admin_collection_path(@lobject_child.lobject_collection), notice: t('.success')
+        @collection = @lobject_child.collection
+        respond_to do |format|
+          format.html { redirect_to edit_unbounded_admin_collection_path(@lobject_child.lobject_collection), notice: t('.success') }
+          format.js
+        end
       end
     end
   end
