718270548e19d14dc6dea2cf0b6bc45183de12ba
RM Saksida <rm@saksida.com.br>
2015-09-14 11:26:12 -0300

    Move Lobject-specific helpers

--- app/helpers/unbounded/lobject_helper.rb
+++ app/helpers/unbounded/lobject_helper.rb
@@ -5,5 +5,17 @@ module Unbounded
     def language_collection_options
       Language.order(:name).map { |lang| [lang.name, lang.id ] }
     end
+
+    def attachment_content_type(download)
+      t("unbounded.content_type.#{download.content_type}") rescue download.content_type
+    end
+
+    def attachment_url(download)
+      if download.url.present?
+        download.url.sub('public://', 'http://k12-content.s3-website-us-east-1.amazonaws.com/')
+      else
+        download.file.url
+      end
+    end
   end
 end
