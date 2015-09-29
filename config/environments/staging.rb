e6c4c40007444f6cc4e9dc5961e5ee13b8906b1f
RM Saksida <rm@saksida.com.br>
2015-09-29 07:59:29 -0300

    Add initialize_on_precompile to environments
    Required by I18n-js

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.assets.initialize_on_precompile = true
 end
