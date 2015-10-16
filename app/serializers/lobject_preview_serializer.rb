46bde48269c21104027ab3e9e1c1e3d29a2c4909
RM Saksida <rm@saksida.com.br>
2015-10-15 21:34:13 -0300

    Truncate long descriptions in resource previews

--- app/serializers/lobject_preview_serializer.rb
+++ app/serializers/lobject_preview_serializer.rb
@@ -1,3 +1,7 @@
 class LobjectPreviewSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :curriculum_subject
+
+  def description
+    object.description.truncate(300, separator: ' ') if object.description
+  end
 end
