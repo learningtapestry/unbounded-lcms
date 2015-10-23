a74cd56fafa4998e2d8f77684d5c89cd15f3fc37
RM Saksida <rm@saksida.com.br>
2015-10-23 17:00:29 -0200

    Track downloads in Google Analytics

--- app/assets/javascripts/unbounded/resource.js
+++ app/assets/javascripts/unbounded/resource.js
@@ -2,6 +2,14 @@
   function initializeResource() {
     if ($('.lesson-page').length) {
       Unbounded.initializePreviews();
+
+      $('.resource-attachment').on('click', function() {
+        ga('send', 'event', {
+          eventCategory: 'download',
+          eventAction: 'resource_attachment',
+          eventLabel: $(this).attr('href')
+        });
+      });
     }
   }
 
