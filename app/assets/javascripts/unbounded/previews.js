9f8933d3d329e2b92ec0a8099376c9cb3f096e0c
RM Saksida <rm@saksida.com.br>
2015-10-23 13:27:42 -0200

    Fix popover positioning

--- app/assets/javascripts/unbounded/previews.js
+++ app/assets/javascripts/unbounded/previews.js
@@ -1,6 +1,80 @@
 (function(window, Unbounded) {  
   var PreviewTemplate;
 
+  function getPlacementFn(defaultPosition, width, height) {
+    return function (tip, element) {
+      var position, top, bottom, left, right;
+
+      var $element = $(element);
+      var boundTop = $(document).scrollTop();
+      var boundLeft = $(document).scrollLeft();
+      var boundRight = boundLeft + $(window).width();
+      var boundBottom = boundTop + $(window).height();
+
+      var pos = $.extend({}, $element.offset(), {
+        width: element.offsetWidth,
+        height: element.offsetHeight
+      });
+
+      var isWithinBounds = function (elPos) {
+        return boundTop < elPos.top && boundLeft < elPos.left && boundRight > (elPos.left + width) && boundBottom > (elPos.top + height);
+      };
+
+      var testTop = function () {
+        if (top === false) return false;
+        top = isWithinBounds({
+            top: pos.top - height,
+            left: pos.left + pos.width / 2 - width / 2
+        });
+        return top ? "top" : false;
+      };
+
+      var testBottom = function () {
+        if (bottom === false) return false;
+        bottom = isWithinBounds({
+            top: pos.top + pos.height,
+            left: pos.left + pos.width / 2 - width / 2
+        });
+        return bottom ? "bottom" : false;
+      };
+
+      var testLeft = function () {
+        if (left === false) return false;
+        left = isWithinBounds({
+            top: pos.top + pos.height / 2 - height / 2,
+            left: pos.left - width
+        });
+        return left ? "left" : false;
+      };
+
+      var testRight = function () {
+        if (right === false) return false;
+        right = isWithinBounds({
+            top: pos.top + pos.height / 2 - height / 2,
+            left: pos.left + pos.width
+        });
+        return right ? "right" : false;
+      };
+
+      switch (defaultPosition) {
+        case "top":
+            if (position = testTop()) return position;
+        case "bottom":
+            if (position = testBottom()) return position;
+        case "left":
+            if (position = testLeft()) return position;
+        case "right":
+            if (position = testRight()) return position;
+        default:
+            if (position = testTop()) return position;
+            if (position = testBottom()) return position;
+            if (position = testLeft()) return position;
+            if (position = testRight()) return position;
+            return defaultPosition;
+      }
+    };
+  }
+
   function initializePopover($elm) {
     $('.popover').popover('hide');
     var popover = $elm.data('bs.popover');
@@ -10,7 +84,7 @@
         content: I18n.t('ui.loading'),
         template: $('#popover-template').html(),
         container: 'body',
-        placement: 'auto right'
+        placement: getPlacementFn('right', 450, 300)
       }).popover('show');
     }
   }
