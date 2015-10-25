cc9e6b34e158c438d5365ba75a02d00d344a488c
Alex <nizamov.alex@gmail.com>
2015-10-25 13:29:12 +0500

    More JS goodness for About page

--- app/assets/javascripts/unbounded/about.js
+++ app/assets/javascripts/unbounded/about.js
@@ -1,6 +1,7 @@
 $(function() {
-  $('.aboutPage__member__close').click(function() {
-    $(this).parent().find('.aboutPage__member__toggle').prop('checked', false);
+  $('.aboutPage__member__label').click(function() {
+    var $toggle = $(this).parent().find('.aboutPage__member__toggle');
+    $toggle.prop('checked', !$toggle.prop('checked'));
     return false;
   });
 });
