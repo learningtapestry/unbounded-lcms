f50f4df7f8f9e269c2081554ed787685a617fee9
Alex <nizamov.alex@gmail.com>
2015-10-25 14:20:58 +0500

    Search: bring back ordering by number of matching standards

--- app/searches/unbounded/lobject_search.rb
+++ app/searches/unbounded/lobject_search.rb
@@ -22,57 +22,70 @@ module Unbounded
           LobjectCollection.curriculum_maps
         end.map(&:id)
 
-      standard_ids = params[:standard_ids].kind_of?(Array) ? params[:standard_ids] : []
+      standard_ids = params[:standard_ids].kind_of?(Array) ? params[:standard_ids].map(&:to_i) : []
 
       search_definition = Content::Search::Esbuilder.build do
         size limit
         from (page - 1) * limit
 
         query do
-          filtered do
-            filter do
-              bool do
-                apply LobjectRestrictions, :restrict_lobjects
+          function_score do
+            functions << {
+              script_score: {
+                params: {
+                  standard_ids: standard_ids
+                },
+                script: '_source.alignments.collect { it.id }.intersect(standard_ids).size()'
+              }
+            }
 
-                must do
-                  nested do
-                    path 'collections'
-                    filter do
-                      terms 'collections.id' => collection_ids
+            query do
+              filtered do
+                filter do
+                  bool do
+                    apply LobjectRestrictions, :restrict_lobjects
+
+                    must do
+                      nested do
+                        path 'collections'
+                        filter do
+                          terms 'collections.id' => collection_ids
+                        end
+                      end
                     end
-                  end
-                end
 
-                if params[:grade_id].present?
-                  must do
-                    nested do
-                      path 'grades'
-                      filter do
-                        term 'grades.id' => params[:grade_id]
+                    if params[:grade_id].present?
+                      must do
+                        nested do
+                          path 'grades'
+                          filter do
+                            term 'grades.id' => params[:grade_id]
+                          end
+                        end
+                      end
+                    end
+
+                    if standard_ids.any?
+                      must do
+                        nested do
+                          path 'alignments'
+                          filter do
+                            terms 'alignments.id' => standard_ids
+                          end
+                        end
                       end
                     end
                   end
                 end
 
-                if standard_ids.any?
-                  must do
-                    nested do
-                      path 'alignments'
-                      filter do
-                        terms 'alignments.id' => standard_ids
-                      end
+                if params[:query].present?
+                  query do
+                    bool do
+                      should { match curriculum_title: { query: params[:query], boost: 4 } }
+                      should { match 'description' => { query: params[:query], boost: 2 } }
+                      should { match '_all' => params[:query] }
                     end
                   end
-                end
-              end
-            end
-
-            if params[:query].present?
-              query do
-                bool do
-                  should { match curriculum_title: { query: params[:query], boost: 4 } }
-                  should { match 'description' => { query: params[:query], boost: 2 } }
-                  should { match '_all' => params[:query] }
                 end
               end
             end
