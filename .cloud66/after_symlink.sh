c2d12a0b833b4fe9a8242f8da9393372fd3f77a8
RM Saksida <rm@saksida.com.br>
2015-12-02 08:52:32 -0200

    Fix Cloud66 permissions issue
    chmod/chown shared folders recursively

--- .cloud66/after_symlink.sh
+++ .cloud66/after_symlink.sh
@@ -1,13 +1,13 @@
 mkdir -p $STACK_BASE/shared/csv_exports
-chown nginx:app_writers $STACK_BASE/shared/csv_exports
-chmod 775 $STACK_BASE/shared/csv_exports
-chmod g+s $STACK_BASE/shared/csv_exports
+chown -R nginx:app_writers $STACK_BASE/shared/csv_exports
+chmod -R 775 $STACK_BASE/shared/csv_exports
+chmod -R g+s $STACK_BASE/shared/csv_exports
 rm -rf $STACK_PATH/public/csv_exports
 ln -nsf $STACK_BASE/shared/csv_exports $STACK_PATH/public/csv_exports
 
 mkdir -p $STACK_BASE/shared/uploads
-chown nginx:app_writers $STACK_BASE/shared/uploads
-chmod 775 $STACK_BASE/shared/uploads
-chmod g+s $STACK_BASE/shared/uploads
+chown -R nginx:app_writers $STACK_BASE/shared/uploads
+chmod -R 775 $STACK_BASE/shared/uploads
+chmod -R g+s $STACK_BASE/shared/uploads
 rm -rf $STACK_PATH/public/uploads
 ln -nsf $STACK_BASE/shared/uploads $STACK_PATH/public/uploads
