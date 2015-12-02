0a993fadf1bdab9a259687214c03b3e9bc6ab961
RM Saksida <rm@saksida.com.br>
2015-12-01 22:38:34 -0200

    Add uploads to Cloud66 shared folders

--- .cloud66/after_symlink.sh
+++ .cloud66/after_symlink.sh
@@ -4,3 +4,10 @@ chmod 775 $STACK_BASE/shared/csv_exports
 chmod g+s $STACK_BASE/shared/csv_exports
 rm -rf $STACK_PATH/public/csv_exports
 ln -nsf $STACK_BASE/shared/csv_exports $STACK_PATH/public/csv_exports
+
+mkdir -p $STACK_BASE/shared/uploads
+chown nginx:app_writers $STACK_BASE/shared/uploads
+chmod 775 $STACK_BASE/shared/uploads
+chmod g+s $STACK_BASE/shared/uploads
+rm -rf $STACK_PATH/public/uploads
+ln -nsf $STACK_BASE/shared/uploads $STACK_PATH/public/uploads
