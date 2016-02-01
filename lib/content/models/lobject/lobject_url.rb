fd48ee4a0d186e7e37ae36dee241662412e2abb6
Alex <nizamov.alex@gmail.com>
2016-02-01 23:57:43 +0500

    [Admin] Add short title to lobject form

--- lib/content/models/lobject/lobject_url.rb
+++ lib/content/models/lobject/lobject_url.rb
@@ -4,7 +4,7 @@ module Content
       belongs_to :lobject
       belongs_to :url
 
-      accepts_nested_attributes_for :url
+      accepts_nested_attributes_for :url, reject_if: ->(attrs) { attrs[:url].blank? }
     end
   end
 end
