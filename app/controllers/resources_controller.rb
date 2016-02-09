9a593e8db4f07c14a1550ec01f2697155f619afb
RM Saksida <rm@saksida.com.br>
2016-02-08 22:50:28 -0200

    Redirect direct links to resources that have slugs

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,7 +2,11 @@ class ResourcesController < ApplicationController
   before_action :find_resource
 
   def show
-    @resource = ResourcePresenter.new(@resource)
+    if params[:id].present? && slug = @resource.slug
+      redirect_to show_with_slug_path(slug), status: 301
+    else
+      @resource = ResourcePresenter.new(@resource)
+    end
   end
 
   def preview
@@ -16,7 +20,10 @@ class ResourcesController < ApplicationController
       elsif slug = params[:slug]
         slug = ResourceSlug.find_by_value!(slug)
         @resource = slug.resource
-        @curriculum = UnboundedCurriculum.new(slug.resource_collection, @resource)
+
+        if collection = slug.resource_collection
+          @curriculum = UnboundedCurriculum.new(collection, @resource)
+        end
       end
     end
 end
