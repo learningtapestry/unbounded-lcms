f68b0062d60bdb56ca5c7998b8cb74dcfebbacbe
RM Saksida <rm@saksida.com.br>
2016-02-16 17:48:00 -0200

    Really fix Cloud66 deploy

--- .cloud66/after_checkout.sh
+++ .cloud66/after_checkout.sh
@@ -1,15 +1,6 @@
 cd $STACK_PATH
-npm set color=false
-npm set progress=false
-npm set loglevel=error
-npm install
-
-chgrp -R ubuntu node_modules
-chown -R ubuntu node_modules
-chmod -R 775 node_modules
-chmod -R g+s node_modules
-
-chgrp -R ubuntu /home/ubuntu/.npm
-chown -R ubuntu /home/ubuntu/.npm
-chmod -R 775 /home/ubuntu/.npm
-chmod -R g+s /home/ubuntu/.npm
+sudo npm install > /tmp/npm.log 2>&1
+sudo chown -R ubuntu $STACK_PATH/node_modules
+sudo chgrp -R ubuntu $STACK_PATH/node_modules
+sudo chown -R ubuntu /home/ubuntu/.npm
+sudo chgrp -R ubuntu /home/ubuntu/.npm
