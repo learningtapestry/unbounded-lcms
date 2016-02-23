693e34fafba1e4edf27e1fefa7f9abdf416c5023
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-02-23 12:55:06 -0300

    lesson#show add more data and placeholders to raw template

--- app/presenters/lesson_presenter.rb
+++ app/presenters/lesson_presenter.rb
@@ -6,4 +6,20 @@ class LessonPresenter < SimpleDelegator
   def unit
     units.first
   end
+
+  def subject
+    unit.subjects.first
+  end
+
+  def grade
+    unit.grades.first
+  end
+
+  def subject_and_grade_title
+    "#{subject.name.titleize} / #{grade.name}"
+  end
+
+  def teaser_text
+    description.truncate_words(25).html_safe
+  end
 end
