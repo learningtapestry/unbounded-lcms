ed517e7b5e66ffaddd0c733e0f491cd92d149f18
Alex <nizamov.alex@gmail.com>
2016-02-24 18:09:07 +0500

    [Google docs] Make popovers accessible

--- app/assets/javascripts/google_docs.js
+++ app/assets/javascripts/google_docs.js
@@ -1,4 +1,26 @@
 window.initializeGoogleDoc = function() {
+  var originalLeave = $.fn.popover.Constructor.prototype.leave;
+
+  $.fn.popover.Constructor.prototype.leave = function(obj) {
+    var self = obj instanceof this.constructor ? obj : $(obj.currentTarget)[this.type](this.getDelegateOptions()).data('bs.' + this.type);
+    var container, timeout;
+
+    originalLeave.call(this, obj);
+
+    if (obj.currentTarget) {
+      container = $(obj.currentTarget).siblings('.popover');
+      timeout = self.timeout;
+      container.one('mouseenter', function() {
+        //We entered the actual popover – call off the dogs
+        clearTimeout(timeout);
+        //Let's monitor popover content instead
+        container.one('mouseleave', function() {
+          $.fn.popover.Constructor.prototype.leave.call(self, self);
+        });
+      });
+    }
+  };
+
   var $googleDoc = $('.googleDoc');
 
   if ($googleDoc.length > 0) {
@@ -15,8 +37,10 @@ window.initializeGoogleDoc = function() {
         var $link = $(sel);
         var $footnote = $link.closest('div').clone();
         $footnote.find('a[href^="#ftnt_ref"]').remove();
+        console.log($footnote.html());
         return $footnote.html();
       },
+      delay: { hide: 500 },
       html: true,
       placement: 'top',
       trigger: 'hover'
