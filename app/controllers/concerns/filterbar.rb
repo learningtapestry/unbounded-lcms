049613df88b46a8c780b3ca2765bcc3c9c1efffc
RM Saksida <rm@saksida.com.br>
2016-02-26 21:30:35 -0300

    Add Filterbar concern

---
+++ app/controllers/concerns/filterbar.rb
@@ -0,0 +1,22 @@
+module Filterbar
+  extend ActiveSupport::Concern
+
+  included do
+    def grade_params
+      Array.wrap(params[:grades]) & [
+        'pk', 'k', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'
+      ]
+    end
+
+    def subject_params
+      Array.wrap(params[:subjects]) & ['ela', 'math']
+    end
+
+    def filterbar_props
+      {
+        subjects: subject_params,
+        grades: grade_params
+      }
+    end
+  end
+end
