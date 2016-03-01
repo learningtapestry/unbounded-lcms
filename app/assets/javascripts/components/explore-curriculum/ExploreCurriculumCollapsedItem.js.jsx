f9b41d299062189fd7b32dbe6a0632f2d3946ff7
RM Saksida <rm@saksida.com.br>
2016-03-01 14:25:13 -0300

    Add `LessonCard` component
    Refactor FindLessons and ExploreCurriculum to use
    shared component

--- app/assets/javascripts/components/explore-curriculum/ExploreCurriculumCollapsedItem.js.jsx
+++ app/assets/javascripts/components/explore-curriculum/ExploreCurriculumCollapsedItem.js.jsx
@@ -1,9 +1,11 @@
 function ExploreCurriculumCollapsedItem(props) {
+  let resource = props.curriculum.resource;
+
   return (
     <div className="o-expcur o-expcur--base">
       <div className="o-expcur__body">
         <div>
-          <strong>{props.curriculum.title}</strong>
+          <strong>{resource.title}</strong>
           &nbsp;<button onClick={props.onClickExpand}>EXPAND</button>
         </div>
       </div>
