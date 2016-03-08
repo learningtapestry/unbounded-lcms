bc91b55b8ed5a18a7fdf47316743994599f81ef0
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-03-08 10:59:23 -0300

    working fuzzy query for resources

--- app/models/search/resources_search.rb
+++ app/models/search/resources_search.rb
@@ -52,8 +52,39 @@ module Search
           end
         end
 
-        def self.search(params)
-          __elasticsearch__.search params
+        def self.build_query(term, options)
+          if term.respond_to?(:to_hash)
+            term
+
+          else
+            limit = options.fetch(:limit, 10)
+            page = options.fetch(:page, 1)
+            {
+              query: {
+                bool: {
+                  should: [
+                    { match: { 'title.full'    => {query: term, type: 'phrase', boost: 5} } },
+                    { match: { 'title.partial' => {query: term, boost: 5} } },
+
+                    { match: { 'subtitle.full'    => {query: term, type: 'phrase', boost: 2} } },
+                    { match: { 'subtitle.partial' => {query: term, boost: 2} } },
+
+                    { match: { 'short_title.full'    => {query: term, type: 'phrase', boost: 2} } },
+                    { match: { 'short_title.partial' => {query: term, boost: 2} } },
+
+                    { match: { 'description.full'    => {query: term, type: 'phrase', boost: 1} } },
+                    { match: { 'description.partial' => {query: term, boost: 1} } },
+                  ]
+                }
+              },
+              size: limit,
+              from: (page - 1) * limit
+            }
+          end
+        end
+
+        def self.search(term, options={})
+          __elasticsearch__.search build_query(term, options)
         end
 
       end
