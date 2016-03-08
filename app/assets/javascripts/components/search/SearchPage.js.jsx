ef7c3b89ffd09a0ff056f7e8fbd069f9601a3e28
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-03-08 15:17:26 -0300

    initial search result page with filterbar

---
+++ app/assets/javascripts/components/search/SearchPage.js.jsx
@@ -0,0 +1,81 @@
+class SearchPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = this.buildStateFromProps(props);
+  }
+
+  buildStateFromProps(props) {
+    return {
+      resources: props.results,
+      per_page: props.pagination.per_page,
+      order: props.pagination.order,
+      current_page: props.pagination.current_page,
+      total_pages: props.pagination.total_pages,
+      num_items: props.pagination.num_items,
+      total_hits: props.pagination.total_hits,
+      filterbar: props.filterbar
+    };
+  }
+
+  fetch() {
+    let query = {
+      format: 'json',
+      per_page: this.state.per_page,
+      order: this.state.order,
+      page: this.state.current_page,
+      ...this.state.filterbar.query
+    }
+    let url = Routes.search_path(query);
+
+    fetch(url).then(r => r.json()).then(response => {
+      this.setState(this.buildStateFromProps(response));
+    });
+  }
+
+  handlePageClick(data) {
+    let selected = data.selected;
+    this.setState(Object.assign({}, this.state, { current_page: selected + 1 }), this.fetch);
+  }
+
+  handleChangePerPage(event) {
+    let newPerPage = event.target.value;
+    this.setState(Object.assign({}, this.state, { per_page: newPerPage }), this.fetch);
+  }
+
+  handleChangeOrder(event) {
+    let newOrder = event.target.value;
+    this.setState(Object.assign({}, this.state, { order: newOrder }), this.fetch);
+  }
+
+  handleFilterbarUpdate(filterbar) {
+    this.setState(Object.assign({}, this.state, { filterbar: filterbar }), this.fetch);
+  }
+
+  render () {
+    return (
+      <div className="o-page__wrap--nest">
+        <Filterbar
+          onUpdate={this.handleFilterbarUpdate.bind(this)}
+          {...this.props.filterbar} />
+        <h2>TODO: results goes here!!</h2>
+        <PaginationBoxView previousLabel={"< Previous"}
+                        nextLabel={"Next >"}
+                        breakLabel={<li className="break"><a href="">...</a></li>}
+                        pageNum={this.state.total_pages}
+                        initialSelected={this.state.current_page - 1}
+                        forceSelected={this.state.current_page - 1}
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={5}
+                        clickCallback={this.handlePageClick.bind(this)}
+                        containerClassName={"o-pagination"}
+                        itemClassName={"o-pagination__item"}
+                        nextClassName={"o-pagination__item--next"}
+                        previousClassName={"o-pagination__item--prev"}
+                        pagesClassName={"o-pagination__item--middle"}
+                        subContainerClassName={"o-pagination__pages"}
+                        activeClassName={"o-pagination__page--active"} />
+       </div>
+     );
+   }
+}
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,31 @@
 class SearchController < ApplicationController
+  include Filterbar
+  include Pagination
+
+  before_action :find_resources
+  before_action :set_props
+
   def index
+    respond_to do |format|
+      format.html
+      format.json { render json: @props }
+    end
   end
+
+  protected
+    def find_resources
+      @resources = Resource.all
+        .paginate(pagination_params.slice(:page, :per_page))
+        .order(created_at: pagination_params[:order])
+        # .where_subject(Subject.from_names(subject_params))
+        # .where_grade(Grade.from_names(grade_params))
+    end
+
+    def set_props
+      @props = serialize_with_pagination(@resources,
+        pagination: pagination_params,
+        each_serializer: LessonSerializer,  # ResourceSerializer
+      )
+      @props.merge!(filterbar_props)
+    end
 end
