d0211ebc0859b65d8dad30eada02b40387a9b053
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-03-10 17:39:12 -0300

    add styling to search input

--- app/assets/javascripts/components/filterbar/FilterbarSearch.js.jsx
+++ app/assets/javascripts/components/filterbar/FilterbarSearch.js.jsx
@@ -17,7 +17,10 @@ class FilterbarSearch extends React.Component {
 
   render() {
     return (
-      <input className='o-filterbar__search' type='text' placeholder='Enter Terms (e.g: writing, geometry, etc)' value={this.state.value} onChange={this.handleChange.bind(this)}/>
+      <div className='o-filterbar__search'>
+        <input className='o-filterbar__search-input' type='search' placeholder='Enter Terms (e.g: writing, geometry, etc)' value={this.state.value} onChange={this.handleChange.bind(this)}/>
+        <i className="fa fa-search o-filterbar__search-icon"></i>
+      </div>
     );
   }
 }
