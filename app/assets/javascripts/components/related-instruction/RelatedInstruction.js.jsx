e71fd5cfc716b509f1daac297a17e031a6f73a94
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-03-15 15:47:09 -0300

    start related instructions component

---
+++ app/assets/javascripts/components/related-instruction/RelatedInstruction.js.jsx
@@ -0,0 +1,14 @@
+class RelatedInstruction extends React.Component {
+  render () {
+    return (
+      <div className="o-related-instruction">
+
+        <h2 className="o-related-instruction__title">Related Instruction</h2>
+
+        <p className="o-related-instruction__teaser">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+        </p>
+      </div>
+     );
+   }
+}
