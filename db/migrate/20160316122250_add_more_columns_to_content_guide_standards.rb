9a9ba8fed75a9e9d0363933cf6ceac10028ad491
RM Saksida <rm@saksida.com.br>
2016-03-23 12:08:26 -0300

    Add default values to null columns

--- db/migrate/20160316122250_add_more_columns_to_content_guide_standards.rb
+++ db/migrate/20160316122250_add_more_columns_to_content_guide_standards.rb
@@ -1,10 +1,10 @@
 class AddMoreColumnsToContentGuideStandards < ActiveRecord::Migration
   def change
     add_column :content_guide_standards, :alt_statement_notation, :string
-    add_column :content_guide_standards, :asn_identifier, :string, null: false
-    add_column :content_guide_standards, :grade, :string, null: false
-    add_column :content_guide_standards, :standard_id, :string, null: false
-    add_column :content_guide_standards, :subject, :string, null: false
+    add_column :content_guide_standards, :asn_identifier, :string, null: false, default: ''
+    add_column :content_guide_standards, :grade, :string, null: false, default: ''
+    add_column :content_guide_standards, :standard_id, :string, null: false, default: ''
+    add_column :content_guide_standards, :subject, :string, null: false, default: ''
 
     rename_column :content_guide_standards, :name, :statement_notation
     change_column :content_guide_standards, :statement_notation, :string, null: true
