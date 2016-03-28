17a00a5aa0c35a37a83177e21bfc8d7745a3e623
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-03-28 00:53:16 -0300

    add redis to deps list on readme + cache explore curriculums controller

--- app/controllers/explore_curriculum_controller.rb
+++ app/controllers/explore_curriculum_controller.rb
@@ -18,20 +18,31 @@ class ExploreCurriculumController < ApplicationController
   end
 
   def set_index_props
-      @curriculums = Curriculum.trees
-        .grades
-        .with_resources
-        .where_subject(subject_params)
-        .where_grade(grade_params)
+      @curriculums = Rails.cache.fetch("explore_curriculums/#{params_cache_key}") do
+        Curriculum.trees
+          .grades
+          .with_resources
+          .where_subject(subject_params)
+          .where_grade(grade_params)
+      end
 
-      @props = ActiveModel::ArraySerializer.new(@curriculums,
-        each_serializer: CurriculumSerializer,
-        root: :results
-      ).as_json.merge!(filterbar_props)
+      @props = Rails.cache.fetch("explore_curriculums/props/#{params_cache_key}") do
+        ActiveModel::ArraySerializer.new(@curriculums,
+          each_serializer: CurriculumSerializer,
+          root: :results
+        ).as_json.merge!(filterbar_props)
+      end
   end
 
   def set_show_props
     @curriculum = Curriculum.find(params[:id])
     @props = CurriculumSerializer.new(@curriculum, depth: 1).as_json
   end
+
+  def params_cache_key
+    grade_key = grade_params.sort.flatten.join(':')
+    subject_key = subject_params.sort.flatten.join(':')
+    "subject::#{subject_key}/grade::#{grade_key}"
+  end
+
 end
