2616923b416bd2fcf28ea466743d8ba0f7994b64
RM Saksida <rm@saksida.com.br>
2016-03-29 08:32:28 -0300

    Fix React server rendering in staging/production
    Ref. https://github.com/reactjs/react-rails/issues/443#issuecomment-180544359

--- config/initializers/react-rails.rb
+++ config/initializers/react-rails.rb
@@ -1,3 +1,9 @@
 Rails.application.config.react.jsx_transform_options = {
   blacklist: ['spec.functionName', 'validation.react'],
 }
+
+# Fixes server rendering in production.
+# Ref. https://github.com/reactjs/react-rails/issues/443#issuecomment-180544359
+Rails.application.config.react.server_renderer_options = {
+  files: ["server_rendering.js"] # files to load for prerendering
+}
