c6c135bc628d32f1b42e4a5727718266ca5699f2
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-03-30 15:47:41 -0300

    modals for explore_curriculum

--- app/assets/javascripts/components/explore-curriculum/ExploreCurriculumExpandedItem.js.jsx
+++ app/assets/javascripts/components/explore-curriculum/ExploreCurriculumExpandedItem.js.jsx
@@ -1,51 +1,56 @@
-function ExploreCurriculumExpandedItem(props) {
-  const resource = props.curriculum.resource;
+class ExploreCurriculumExpandedItem extends React.Component {
+  render() {
+    var props = this.props;
 
-  const curriculumComponent = {
-    'grade': ExploreCurriculumGradeMap,
-    'module': ExploreCurriculumModuleMap,
-    'unit': ExploreCurriculumUnitMap,
-    'lesson': ExploreCurriculumGradeMap
-  }[props.curriculum.type];
+    const resource = props.curriculum.resource;
 
-  const curriculumMap = React.createElement(curriculumComponent, {
-    expanded: true,
-    onClickDetails: props.onClickViewDetails,
-    curriculum: props.curriculum
-  });
+    const curriculumComponent = {
+      'grade': ExploreCurriculumGradeMap,
+      'module': ExploreCurriculumModuleMap,
+      'unit': ExploreCurriculumUnitMap,
+      'lesson': ExploreCurriculumGradeMap
+    }[props.curriculum.type];
 
-  const cssClasses = classNames(
-    'o-cur-card',
-    `o-cur-card--${props.curriculum.type}`
-  );
+    const curriculumMap = React.createElement(curriculumComponent, {
+      expanded: true,
+      onClickDetails: props.onClickViewDetails,
+      curriculum: props.curriculum
+    });
 
-  const description = { __html: resource.description };
+    const cssClasses = classNames(
+      'o-cur-card',
+      `o-cur-card--${props.curriculum.type}`
+    );
 
-  const resourceType = resource.type.name == 'grade' ? 'curriculum' : resource.type.name;
-  const downloadBtnLabel = `Download ${_.capitalize(resourceType)}`;
+    const description = { __html: resource.description };
 
-  return (
-    <div id={props.curriculum.id} className={cssClasses}>
-      {curriculumMap}
-      <div className="o-cur-card__body" onClick={props.onClickViewDetails}>
-        <div className="o-title">
-          <span className="o-title__type">{resource.short_title}</span>
-          <span className="o-title__duration">{resource.time_to_teach} min</span>
+    const resourceType = resource.type.name == 'grade' ? 'curriculum' : resource.type.name;
+    const downloadBtnLabel = `Download ${_.capitalize(resourceType)}`;
+    const downloadModalId = `downloads-modal-${resource.id}`
+
+    return (
+      <div id={props.curriculum.id} className={cssClasses}>
+        {curriculumMap}
+        <div className="o-cur-card__body" onClick={props.onClickViewDetails}>
+          <div className="o-title">
+            <span className="o-title__type">{resource.short_title}</span>
+            <span className="o-title__duration">{resource.time_to_teach} min</span>
+          </div>
+          <h2>{resource.title}</h2>
+          <div className="u-html-description" dangerouslySetInnerHTML={description}></div>
         </div>
-        <h2>{resource.title}</h2>
-        <div className="u-html-description" dangerouslySetInnerHTML={description}></div>
+        <div className="o-cur-card__actions">
+          <div>
+            <a className="o-ub-btn" href={resource.path}>View Details</a>
+          </div>
+          <div>
+            <button className="o-ub-btn o-ub-btn--bordered" data-open={downloadModalId}>{downloadBtnLabel}</button>
+          </div>
+          <div>
+            <button className="o-ub-btn o-ub-btn--bordered">Related Instruction</button>
+          </div>
+        </div>
       </div>
-      <div className="o-cur-card__actions">
-        <div>
-          <a className="o-ub-btn" href={resource.path}>View Details</a>
-        </div>
-        <div>
-          <button className="o-ub-btn o-ub-btn--bordered">{downloadBtnLabel}</button>
-        </div>
-        <div>
-          <button className="o-ub-btn o-ub-btn--bordered">Related Instruction</button>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
--- app/assets/javascripts/components/explore-curriculum/ExploreCurriculumItem.js.jsx
+++ app/assets/javascripts/components/explore-curriculum/ExploreCurriculumItem.js.jsx
@@ -1,50 +1,79 @@
-function ExploreCurriculumItem(props) {
+class ExploreCurriculumItem extends React.Component {
 
-  const curriculum = props.index[props.id];
+  curriculum() {
+    return this.props.index[this.props.id];
+  }
 
-  // An item should expand if its parent is the last parent in the active branch.
-  // Collapsed Grade -> Collapsed Mod -> Expanded Unit 1, Unit 2, Unit 3
-  const activeParent = props.index[props.active[props.active.length - 2]];
-  const shouldItemExpand = props.active.length === 1 ||
-    _.some(activeParent.children, c => c.id === props.id);
+  modalId() {
+    const resource = this.curriculum().resource;
+    return `downloads-modal-${resource.id}`
+  }
 
-  const item = shouldItemExpand ?
-    <ExploreCurriculumExpandedItem
-      curriculum={curriculum}
-      onClickViewDetails={props.onClickViewDetails.bind(this, props.parentage)} /> :
-    <ExploreCurriculumCollapsedItem
-      curriculum={curriculum}
-      onClickExpand={props.onClickExpand.bind(this, props.parentage)} />;
-
-  // Children should be rendered if the item is a parent in the active branch.
-  const shouldRenderChildren = props.active.length > 1 &&
-    _.includes(props.active, props.id) &&
-    _.last(props.active) !== props.id;
-
-  const cssClasses = classNames( 'c-ec-cards__children',
-                               { 'c-ec-cards__children--lessons': curriculum.type === 'unit',
-                                 'c-ec-cards__children--expanded': shouldRenderChildren });
-
-  const children = shouldRenderChildren ?
-    curriculum.children.map(c => (
-      c.type === 'lesson' ?
-        <LessonCard key={c.resource.id} lesson={c.resource} type="light" /> :
-        <ExploreCurriculumItem
-          key={c.id}
-          id={c.id}
-          index={props.index}
-          onClickExpand={props.onClickExpand}
-          onClickViewDetails={props.onClickViewDetails}
-          parentage={[...props.parentage, c.id]}
-          active={props.active} />
-    )) : [];
-
-  return (
-    <div>
-      {item}
-      <div className={cssClasses}>
-        {children}
+  modal() {
+    return (
+      <div className="o-download-modal" id={this.modalId()} data-reveal>
+        <button className="close-button" data-close aria-label="Close modal" type="button">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        downloads for resource {this.curriculum().resource.title}
       </div>
-    </div>
-  );
+    );
+  }
+
+  componentDidMount() {
+    const modalEl = $(`#${this.modalId()}`);
+    new Foundation.Reveal(modalEl);
+  }
+
+  render() {
+    const props = this.props;
+    const curriculum = this.curriculum();
+
+    // An item should expand if its parent is the last parent in the active branch.
+    // Collapsed Grade -> Collapsed Mod -> Expanded Unit 1, Unit 2, Unit 3
+    const activeParent = props.index[props.active[props.active.length - 2]];
+    const shouldItemExpand = props.active.length === 1 ||
+      _.some(activeParent.children, c => c.id === props.id);
+
+    const item = shouldItemExpand ?
+      <ExploreCurriculumExpandedItem
+        curriculum={curriculum}
+        onClickViewDetails={props.onClickViewDetails.bind(this, props.parentage)} /> :
+      <ExploreCurriculumCollapsedItem
+        curriculum={curriculum}
+        onClickExpand={props.onClickExpand.bind(this, props.parentage)} />;
+
+    // Children should be rendered if the item is a parent in the active branch.
+    const shouldRenderChildren = props.active.length > 1 &&
+      _.includes(props.active, props.id) &&
+      _.last(props.active) !== props.id;
+
+    const cssClasses = classNames( 'c-ec-cards__children',
+                                 { 'c-ec-cards__children--lessons': curriculum.type === 'unit',
+                                   'c-ec-cards__children--expanded': shouldRenderChildren });
+
+    const children = shouldRenderChildren ?
+      curriculum.children.map(c => (
+        c.type === 'lesson' ?
+          <LessonCard key={c.resource.id} lesson={c.resource} type="light" /> :
+          <ExploreCurriculumItem
+            key={c.id}
+            id={c.id}
+            index={props.index}
+            onClickExpand={props.onClickExpand}
+            onClickViewDetails={props.onClickViewDetails}
+            parentage={[...props.parentage, c.id]}
+            active={props.active} />
+      )) : [];
+
+    return (
+      <div>
+        {item}
+        <div className={cssClasses}>
+          {children}
+        </div>
+        {this.modal()}
+      </div>
+    );
+  }
 }
