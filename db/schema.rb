27432ab37eef3973741740a261b64d620aa1dd12
RM Saksida <rm@saksida.com.br>
2016-04-06 10:34:40 -0300

    Add breadcrumbs to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330231857) do
+ActiveRecord::Schema.define(version: 20160406124444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,12 +59,16 @@ ActiveRecord::Schema.define(version: 20160330231857) do
   end
 
   create_table "curriculums", force: :cascade do |t|
-    t.integer "curriculum_type_id", null: false
+    t.integer "curriculum_type_id",     null: false
     t.integer "parent_id"
     t.integer "position"
-    t.integer "item_id",            null: false
-    t.string  "item_type",          null: false
+    t.integer "item_id",                null: false
+    t.string  "item_type",              null: false
     t.integer "seed_id"
+    t.string  "breadcrumb_title"
+    t.string  "breadcrumb_short_title"
+    t.string  "breadcrumb_piece"
+    t.string  "breadcrumb_short_piece"
   end
 
   add_index "curriculums", ["curriculum_type_id"], name: "index_curriculums_on_curriculum_type_id", using: :btree
