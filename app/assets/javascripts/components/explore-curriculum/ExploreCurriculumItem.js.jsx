7946ebc23eda796ec009341cdc16e0ab1c2ea92a
shlag3n <Oksana.Melnikova@gmail.com>
2016-04-08 15:12:15 +0300

    changed accroding to feedback

--- app/assets/javascripts/components/explore-curriculum/ExploreCurriculumItem.js.jsx
+++ app/assets/javascripts/components/explore-curriculum/ExploreCurriculumItem.js.jsx
@@ -3,68 +3,6 @@ class ExploreCurriculumItem extends React.Component {
     return this.props.index[this.props.id];
   }
 
-  modalId() {
-    const resource = this.curriculum().resource;
-    return `downloads-modal-${resource.id}`;
-  }
-
-  download(download) {
-    const cls = `fa fa-lg file-${download.icon}`;
-    return `<li>
-              <i class="${cls}"></i>
-              <span>
-                <a href=${download.url} class="resource-attachment" target="_blank">${download.title}</a>
-              </span>
-            </li>`;
-  }
-
-  modalContentData() {
-    const resource = this.curriculum().resource;
-    return `<h2>Download ${_.capitalize(resource.type.name)}</h2>
-            <div class="o-download-modal__content">
-              <ul class="o-resource__list o-resource__list--icons">
-                ${ _.map(resource.downloads, item => this.download(item)).join('\n') }
-              </ul>
-            </div>
-            <button class="close-button" data-close aria-label="Close modal" type="button">
-              <span aria-hidden="true">&times;</span>
-            </button>`;
-  }
-
-  modalContent() {
-    return { __html: this.modalContentData() };
-  }
-
-  modal() {
-    const resource = this.curriculum().resource;
-    return (resource.downloads && resource.downloads.length > 0) ?
-      (
-        <div className="o-download-modal" ref="modal" id={this.modalId()}
-             data-reveal dangerouslySetInnerHTML={ this.modalContent() }>
-        </div>
-      ) : '';
-  }
-
-  onDownloads(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    if (this.refs.modal) {
-      $(this.refs.modal).foundation('open');
-    }
-  }
-
-  componentDidMount() {
-    if (this.refs.modal) {
-      new Foundation.Reveal($(this.refs.modal));
-    }
-  }
-
-  componentWillUnmount() {
-    if (this.refs.modal) {
-      //$(this.refs.modal).foundation('destroy');
-    }
-  }
-
   render() {
     const props = this.props;
     const curriculum = this.curriculum();
@@ -79,7 +17,6 @@ class ExploreCurriculumItem extends React.Component {
       <ExploreCurriculumCardItem
         curriculum={curriculum}
         onClickElement={ shouldItemExpand ? props.onClickViewDetails.bind(this, props.parentage) : props.onClickExpand.bind(this, props.parentage)}
-        onDownloads={this.onDownloads.bind(this)}
         shouldItemExpand={shouldItemExpand}/>;
 
     // Children should be rendered if the item is a parent in the active branch.
@@ -112,7 +49,7 @@ class ExploreCurriculumItem extends React.Component {
         <div className={cssClasses}>
             {children}
         </div>
-        {this.modal()}
+        <DownloadModal {...curriculum.resource} />
       </div>
       );
    }
