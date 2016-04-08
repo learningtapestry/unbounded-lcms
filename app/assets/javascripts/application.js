13cf5c48a0f780111f3a74d5e598178b694dd1de
shlag3n <Oksana.Melnikova@gmail.com>
2016-04-08 17:19:59 +0300

    (#97, 99) changed according to feedback

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,25 +2,22 @@
 //= require ./routes
 //= require jquery
 //= require jquery_ujs
+//= require ready
 //= require jquery/smoothscrolling
 //= require foundation
 //= require jquery/foundation.magellanex
+//= require jquery/foundation.initialize
 //= require react
 //= require react_ujs
 //= require components
 //= require content_guides
 
-function ready() {
-  $(document).foundation();
-  $(document).smoothscrolling();
-  window.initializeContentGuide();
-}
+$(function () {
+  function initialize() {
+    $(document).initFoundation();
+    $(document).smoothscrolling();
+    window.initializeContentGuide();
+  }
 
-function readyTurbolinks() {
-  ready();
-  // initialize foundation event listeners (they are initializing on window.load)
-  Foundation.IHearYou();
-}
-
-$(document).on('ready', ready);
-$(document).on('page:load', readyTurbolinks);
+  ready(initialize);
+})
