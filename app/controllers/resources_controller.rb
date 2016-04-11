6e8a31fbf2ccc8635979eda0e2a4e0dae6f66443
shlag3n <Oksana.Melnikova@gmail.com>
2016-04-11 21:29:34 +0300

    fixed tests

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -25,7 +25,8 @@ class ResourcesController < ApplicationController
       end
 
       @resource = ResourcePresenter.new(resource)
-      @color_code = @resource.subject + '-' + @resource.grade_type
+      @color_code = (@resource.subject.present? && @resource.grade_type.present?) ?
+                     @resource.subject + '-' + @resource.grade_type : 'base'
       @curriculum = CurriculumPresenter.new(curriculum)
     end
 end
