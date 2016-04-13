a3b36515c5affd5f08652371d5dfbfacf0db106d
RM Saksida <rm@saksida.com.br>
2016-04-12 21:28:11 -0300

    Add breadcrumbs to Find Lessons cards

--- app/assets/javascripts/components/lesson-card/LessonCard.js.jsx
+++ app/assets/javascripts/components/lesson-card/LessonCard.js.jsx
@@ -5,7 +5,7 @@ function LessonCard(props) {
     <a className="o-lesson-card o-lesson-card--base" href={url}>
       <div className={`cs-bg--${props.colorCode}`}>
         <div className="o-lesson-card__header o-title cs-txt--white">
-          <span className="o-title__type">{props.lesson.short_title}</span>
+          <span className="o-title__type">{props.lesson.breadcrumb_title}</span>
         </div>
         <div className="o-lesson-card__content o-lesson-card__content--base cs-bg--white">
           <h3 className="o-lesson-card__dsc--short">{props.lesson.title}</h3>
