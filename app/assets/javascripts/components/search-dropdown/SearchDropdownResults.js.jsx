32c9296fdd390c513a0646520dc98fc7a173751d
RM Saksida <rm@saksida.com.br>
2016-04-21 16:32:38 -0300

    Improve search dropdown styling

--- app/assets/javascripts/components/search-dropdown/SearchDropdownResults.js.jsx
+++ app/assets/javascripts/components/search-dropdown/SearchDropdownResults.js.jsx
@@ -1,11 +1,12 @@
 function SearchDropdownResults(props) {
   if (props.resources.length == 0) {
+    const cls = 'o-search-dropdown-result';
     if (props.isSearching) {
-      return <div>Searching...</div>;
+      return <div className={cls}>Searching...</div>;
     } else if (props.search_term && props.search_term.length > 0) {
-      return <div>Nothing found.</div>;
+      return <div className={cls}>Nothing found.</div>;
     }
-    return <div>Enter terms to start searching.</div>;
+    return <div className={cls}>Enter terms to start searching.</div>;
   }
 
   const results = props.resources.map((resource, idx) => {
