70ad5e57daf3662294c944d32a0b66d7dcf55e70
Alex <nizamov.alex@gmail.com>
2016-04-25 19:53:50 +0500

    [Content guides] Do not style table with single cell

--- app/presenters/content_guide_presenter.rb
+++ app/presenters/content_guide_presenter.rb
@@ -310,9 +310,11 @@ class ContentGuidePresenter < BasePresenter
 
   def reset_table_styles
     doc.css('table').each do |table|
+      next if table.xpath('tbody/tr/td').size == 1
+
       table[:class] = 'c-cg-table'
       table.remove_attribute('style')
-      table.css('[style]').each do |node|
+      table.css('tr, td').each do |node|
         node.remove_attribute('style')
       end
     end
