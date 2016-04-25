3394d0d076b554fbe292a9b839df65173a9f7795
shlag3n <Oksana.Melnikova@gmail.com>
2016-04-25 16:49:49 +0300

    (#146, #143, #111) styled enhance instruction, changed fields to show on instruction cards

--- app/serializers/video_instruction_serializer.rb
+++ app/serializers/video_instruction_serializer.rb
@@ -2,28 +2,15 @@ class VideoInstructionSerializer < ActiveModel::Serializer
   include ResourceHelper
 
   self.root = false
-  attributes :id, :title, :short_title, :subtitle, :subject, :teaser, :img, :path, :instruction_type, :time_to_teach
-
-  def short_title
-    object.short_title || default_short_title
-  end
+  attributes :id, :title, :subject, :teaser, :img, :path, :instruction_type
 
   def subject
     object.try(:subject).downcase || 'default'
   end
 
-  def default_short_title
-    "#{object.subject.titleize} #{object.grades.first.try(:name).try(:titleize)} #{instruction_type.try(:titleize)}"
-  end
-
   def img
     # TODO fix-me when we add photos/images to Resources
     ActionController::Base.helpers.image_path('placeholder_white_bg.svg')
-  end
-
-  def time_to_teach
-    # TODO fix-me when we add photos/images to Resources
-    60
   end
 
   def path
