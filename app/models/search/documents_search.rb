91cc1f975c1f7d7e289dbf49c3b467b6f063cc62
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-04-26 14:35:01 -0300

    multi type search for resource and content_guides

---
+++ app/models/search/documents_search.rb
@@ -0,0 +1,39 @@
+require 'elasticsearch/model'
+
+module Search
+  module DocumentsSearch
+    module_function
+
+    def build_query(term, options)
+      if term.respond_to?(:to_hash)
+        term
+
+      else
+        limit = options.fetch(:limit, 10)
+        page = options.fetch(:page, 1)
+        {
+          query: {
+            bool: {
+              should: [
+                { match: { 'title.full'    => {query: term, type: 'phrase', boost: 5} } },
+                { match: { 'title.partial' => {query: term, boost: 5} } },
+
+                { match: { 'description.full'    => {query: term, type: 'phrase', boost: 1} } },
+                { match: { 'description.partial' => {query: term, boost: 1} } },
+              ]
+            }
+          },
+          size: limit,
+          from: (page - 1) * limit
+        }
+      end
+    end
+
+    def search(term, models=nil, options={})
+      models ||= [ ContentGuide, Resource ]
+      query = build_query(term, options)
+      Elasticsearch::Model.search(query, models)
+    end
+
+  end
+end
