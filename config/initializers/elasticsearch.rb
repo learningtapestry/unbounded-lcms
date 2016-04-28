3b5818faddde4d24a02b90309d199086c4b205b5
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-04-28 02:03:33 -0300

    extend properly es search results to handle pagination

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,5 @@
 require 'elasticsearch/model'
+require "#{Rails.root}/lib/elasticsearch/persistence/repository/response/results"
 
 Elasticsearch::Model.client = Elasticsearch::Client.new(
   host: ENV['ELASTICSEARCH_ADDRESS']
