a22a8fd8879351ccf5679165b0e49f085787573a
shlag3n <Oksana.Melnikova@gmail.com>
2016-04-29 16:50:45 +0300

    (#161) ui minor changes

--- app/presenters/resource_presenter.rb
+++ app/presenters/resource_presenter.rb
@@ -1,15 +1,6 @@
 class ResourcePresenter < SimpleDelegator
-  def tags
-    super.sort.join(', ')
-  end
-
   def color_code(grade_color_code = 'base')
     subject_color_code = try(:subject) || 'default'
     "#{subject_color_code}-#{grade_color_code}"
   end
-
-  protected
-    def h
-      ApplicationController.helpers
-    end
 end
