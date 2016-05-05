1c583c1a65634dfe1d3c1f0907d7752d5eb1486a
Alex <nizamov.alex@gmail.com>
2016-05-05 19:49:02 +0500

    Check if standard is assigned to resource when seeding standards

--- db/seeds/resources_unbounded_standards.seeds.rb
+++ db/seeds/resources_unbounded_standards.seeds.rb
@@ -1,10 +1,11 @@
 # Math Grade 6 Module 1
 if (resource = ResourceSlug.find_by_value('math/grade-6/module-1').try(:resource))
-  resource.unbounded_standards << UnboundedStandard.create_with(subject: 'math').find_or_create_by!(name: 'm-g6')
+  standard = UnboundedStandard.create_with(subject: 'math').find_or_create_by!(name: 'm-g6')
+  resource.unbounded_standards << standard unless resource.unbounded_standards.include?(standard)
 end
 
 # ELA K, 1, 2 (all units)
 standard = UnboundedStandard.create_with(subject: 'ela').find_or_create_by!(name: 'ela-k2')
 Curriculum.ela.where_grade(['kindergarten', 'grade 1', 'grade 2']).units.includes(:item).map(&:resource).uniq.each do |resource|
-  resource.unbounded_standards << standard
+  resource.unbounded_standards << standard unless resource.unbounded_standards.include?(standard)
 end
