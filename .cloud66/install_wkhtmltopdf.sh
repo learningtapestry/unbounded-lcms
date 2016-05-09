307a146066361edf41d39630f41ed971d54bd45a
Alex <nizamov.alex@gmail.com>
2016-05-09 18:57:48 +0500

    Use env var in `install_wkhtmltopdf` hook

--- .cloud66/install_wkhtmltopdf.sh
+++ .cloud66/install_wkhtmltopdf.sh
@@ -1,5 +1,5 @@
-if [ ! -f /usr/local/bin/wkhtmltopdf ]; then
+if [ ! -f $WKHTMLTOPDF_PATH ]; then
   wget http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
   tar xvfJ wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
-  cp wkhtmltox/bin/wkhtmltopdf /usr/local/bin
+  cp wkhtmltox/bin/wkhtmltopdf $WKHTMLTOPDF_PATH
 fi
