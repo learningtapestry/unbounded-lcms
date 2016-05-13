c2cc0e75a45b13916fd7079f17478cd22e4a8380
Alex <nizamov.alex@gmail.com>
2016-05-13 19:21:39 +0500

    [Content guides] Store OAuth2 tokens in Redis

--- app/controllers/concerns/google_auth.rb
+++ app/controllers/concerns/google_auth.rb
@@ -1,4 +1,4 @@
-require 'googleauth/stores/file_token_store'
+require 'googleauth/stores/redis_token_store'
 require 'googleauth/web_user_authorizer'
 
 module GoogleAuth
@@ -9,10 +9,10 @@ module GoogleAuth
   def obtain_google_credentials
     client_id = Google::Auth::ClientId.new(ENV['GOOGLE_OAUTH2_CLIENT_ID'], ENV['GOOGLE_OAUTH2_CLIENT_SECRET'])
     scope = %w(https://www.googleapis.com/auth/drive)
-    token_store = Google::Auth::Stores::FileTokenStore.new(file: 'tmp/cache/tokens.yml')
+    token_store = Google::Auth::Stores::RedisTokenStore.new(redis: Redis.new(url: ENV.fetch('REDIS_URL', 'redis://localhost:6379')))
     authorizer = Google::Auth::WebUserAuthorizer.new(client_id, scope, token_store, admin_google_oauth2_callback_path)
 
-    credentials = authorizer.get_credentials(current_user.id, request)
+    credentials = authorizer.get_credentials(current_user.id.to_s, request)
 
     if credentials
       @google_credentials = credentials
