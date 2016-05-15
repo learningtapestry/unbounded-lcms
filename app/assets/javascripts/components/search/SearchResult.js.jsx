aa9d94d9790fecd8c925ece0ae62452f905150eb
shlag3n <Oksana.Melnikova@gmail.com>
2016-05-15 11:35:12 +0300

    (#204) styled search + about people pages changed google analytics init to be compatible with turbolinks

--- app/assets/javascripts/components/search/SearchResult.js.jsx
+++ app/assets/javascripts/components/search/SearchResult.js.jsx
@@ -1,11 +1,18 @@
 function SearchResult(props) {
+  const breadcrumb = props.resource.breadcrumbs ?
+          <span className='o-search-results__subheader'><span className='s-bullet'></span>{props.resource.breadcrumbs}</span>
+          : null;
+  const colorCode = colorCodeCss(props.resource.subject, props.resource.grade);
   return (
     <li className='o-search-results__item'>
-      <div className='o-search-results__item--type'>{props.resource.type_name}</div>
-      <div className='o-search-results__item--title'>
-        <a href={props.resource.path}>{props.resource.title}</a>
+      <div className={`cs-txt--${colorCode}`}>
+        <span className='o-search-results__header'>{props.resource.type_name}</span>
+        {breadcrumb}
       </div>
-      <div className='o-search-results__item--teaser'>
+      <div className='o-search-results__title'>
+        <a href={props.resource.path}><h3>{props.resource.title}</h3></a>
+      </div>
+      <div className='o-search-results__teaser'>
         {props.resource.teaser}
       </div>
     </li>
