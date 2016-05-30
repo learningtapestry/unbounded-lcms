009fb7992dec496eced469bd6f1149db9e5806c8
Alex <nizamov.alex@gmail.com>
2016-05-30 23:07:39 +0500

    Fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -45,12 +45,12 @@ ActiveRecord::Schema.define(version: 20160525085837) do
   add_index "content_guide_standards", ["standard_id"], name: "index_content_guide_standards_on_standard_id", using: :btree
 
   create_table "content_guides", force: :cascade do |t|
-    t.string   "content",                                   null: false
-    t.string   "file_id",                                   null: false
-    t.string   "name",                                      null: false
-    t.string   "original_content",                          null: false
-    t.datetime "created_at",                                null: false
-    t.datetime "updated_at",                                null: false
+    t.string   "content",                   null: false
+    t.string   "file_id",                   null: false
+    t.string   "name",                      null: false
+    t.string   "original_content",          null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.datetime "last_modified_at"
     t.string   "last_modifying_user_email"
     t.string   "last_modifying_user_name"
@@ -62,8 +62,6 @@ ActiveRecord::Schema.define(version: 20160525085837) do
     t.string   "subject"
     t.string   "teaser"
     t.string   "title"
-    t.boolean  "fake",                      default: false, null: false
-    t.string   "mailchimp_list_id"
     t.string   "permalink"
     t.string   "slug"
   end
