a1e45b4d86ea0843b074867a5da4cf63fcf240fb
shlag3n <Oksana.Melnikova@gmail.com>
2016-06-05 14:12:05 +0300

    (#284, #285, #286) minor ui changes

--- app/assets/javascripts/components/find-lessons/FindLessonsCardsEmpty.js.jsx
+++ app/assets/javascripts/components/find-lessons/FindLessonsCardsEmpty.js.jsx
@@ -5,7 +5,7 @@ function FindLessonsCardsEmpty(props) {
 
       <p>Please try searching a topic (e.g. volume), a text or author (e.g. Macbeth or Frederick Douglass), or a standard (e.g. 2.NBT.A.2).</p>
 
-      <h3 className='u-text--large'>Sugestions</h3>
+      <h3 className='u-text--large'>Suggestions</h3>
       <ul>
         <li>Check your spelling</li>
         <li>Try using fewer filters</li>
