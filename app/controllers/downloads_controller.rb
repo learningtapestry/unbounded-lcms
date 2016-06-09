444a52330c8df9eff1900c377f094e5593128991
RM Saksida <rm@saksida.com.br>
2016-06-09 08:44:42 -0300

    Add slug to GA download event

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -2,11 +2,21 @@ class DownloadsController < ApplicationController
   def show
     @download = Download.find(params[:id])
 
+    category = 'download'
+
+    action = if params[:slug_id]
+      "https://www.unbounded.org#{show_with_slug_path(ResourceSlug.find(params[:slug_id]).value)}"
+    else
+      nil
+    end
+
+    label = @download.attachment_url
+
     tracker = Staccato.tracker(ENV['GOOGLE_ANALYTICS_ID'])
     tracker.event(
-      category: 'download',
-      action: @download.attachment_content_type,
-      label: @download.attachment_url
+      category: category,
+      action: action,
+      label: label
     )
 
     redirect_to @download.attachment_url
