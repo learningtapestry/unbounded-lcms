7fb2b8458fd950ce84b7e633c6d547845597334f
RM Saksida <rm@saksida.com.br>
2016-06-16 18:40:35 -0300

    Fix bug in video list item
    Use `padLeft`, not `pad`

--- app/assets/javascripts/components/helpers/MediaTime.js.jsx
+++ app/assets/javascripts/components/helpers/MediaTime.js.jsx
@@ -2,5 +2,5 @@ function MediaTime(props) {
   const duration = props.duration || 0;
   const minutes = Math.floor(duration/60),
         seconds = duration%60;
-  return <span>{`${_.pad(minutes, 2, '0')}:${_.pad(seconds, 2, '0')}`}</span>
+  return <span>{`${_.padLeft(minutes, 2, '0')}:${_.padLeft(seconds, 2, '0')}`}</span>
 }
