716c975a471ffb4f410aa4273c8819fde8b270fb
RM Saksida <rm@saksida.com.br>
2016-06-16 13:39:14 -0300

    Update resource attachment uploader
    Set content disposition for resource attachments

--- app/uploaders/download_uploader.rb
+++ app/uploaders/download_uploader.rb
@@ -4,4 +4,8 @@ class DownloadUploader < CarrierWave::Uploader::Base
   def store_dir
     "attachments/#{model.id}"
   end
+
+  def fog_attributes
+    { 'Content-Disposition' => 'attachment' }
+  end
 end
