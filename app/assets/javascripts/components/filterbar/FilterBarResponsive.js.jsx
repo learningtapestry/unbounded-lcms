b53dcfa0665205680cebbf20be00f68c0b015ebd
shlag3n <Oksana.Melnikova@gmail.com>
2016-06-24 23:47:53 +0300

    (#301, #288) styled pdf + fixed issue with close button for filter

--- app/assets/javascripts/components/filterbar/FilterBarResponsive.js.jsx
+++ app/assets/javascripts/components/filterbar/FilterBarResponsive.js.jsx
@@ -45,7 +45,7 @@ class FilterbarResponsive extends React.Component {
                {...this.props}
                 withDropdown={false}
                 onRefine={this.handleRefineClick.bind(this)} />
-            <button className="close-button" data-close aria-label="Close modal" type="button" onClick={this.handleRefineClick.bind(this)}>
+            <button className="close-button" aria-label="Close modal" type="button" onClick={this.handleRefineClick.bind(this)}>
               <span aria-hidden="true"><i className="ub-close"></i></span>
             </button>
           </div>
