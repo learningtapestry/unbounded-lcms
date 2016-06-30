c76c1eba1208f15a3b2de1e257eb2a35c14900d6
RM Saksida <rm@saksida.com.br>
2016-06-30 12:07:16 -0300

    Fix base64 encoded assets resolution
    Always use "base64_encoded_asset" helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,4 +57,21 @@ module ApplicationHelper
     content_for(:og_image, image_url) if image_url.present?
   end
 
+  def base64_encoded_asset(path)
+    asset, content_type = if Rails.env.development?
+      asset = Rails.application.assets.find_asset(path)
+      content_type = asset.content_type
+      [asset, content_type]
+    else
+      filesystem_path = Rails.application.assets_manifest.assets[path]
+      asset = File.read(Rails.root.join('public', 'assets', filesystem_path))
+      content_type = Mime::Type.lookup_by_extension(File.extname(path).split('.').last)
+      [asset, content_type]
+    end
+    raise "Could not find asset '#{path}'" if asset.nil?
+    puts Rails.application.assets
+    base64 = Base64.encode64(asset.to_s).gsub(/\s+/, '')
+    "data:#{content_type};base64,#{Rack::Utils.escape(base64)}"
+  end
+
 end
--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,3 @@
 WickedPdf.config = {
   exe_path: ENV['WKHTMLTOPDF_PATH']
 }
-
-require "#{Rails.root}/lib/wicked_pdf/wicked_pdf_helper/assets.rb"
