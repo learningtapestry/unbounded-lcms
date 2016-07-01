3764cb646124b554eff4cad22527b90955b9338b
shlag3n <Oksana.Melnikova@gmail.com>
2016-07-01 19:58:13 +0300

    (#306) added leadeship page title

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160701092151) do
-=======
-ActiveRecord::Schema.define(version: 20160630135052) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
