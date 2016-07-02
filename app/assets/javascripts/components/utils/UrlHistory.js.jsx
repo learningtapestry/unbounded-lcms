5468b5c1816239f810c4a2eaa6fae611d5f0cc0c
RM Saksida <rm@saksida.com.br>
2016-07-02 16:03:17 -0300

    Improve UrlHistory
    Construct URL state from the query string if we're
    starting out with a blank state.

--- app/assets/javascripts/components/utils/UrlHistory.js.jsx
+++ app/assets/javascripts/components/utils/UrlHistory.js.jsx
@@ -2,10 +2,45 @@ urlHistory = (function() {
 
   let state = {};
 
+  let hasStateBeenConstructedFromTheUrl = false;
+
   let update = function(newState, options) {
     // options:
     //   skipKey: function which returns if a key should be skiped or not
     //   replace: if true uses replaceState instead the default pushState
+
+    // Initialize state from the query string.
+    if (!hasStateBeenConstructedFromTheUrl) {
+      let qs = window.location.search.slice(1); // Remove leading '?'.
+      let pairs = qs.split('&');
+
+      _.forEach(pairs, function(pair) {
+        pair = pair.split('=');
+        let key = pair[0];
+        let values = decodeURIComponent(pair[1] || '').split(',');
+
+        if( state[key] ) {
+          // This is an array encoded as repetitive query string values.
+          // param=1&param=2&param=3
+          if(_.isArray(state[key])) {
+            state[key] = _.union(state[key], values);
+          } else {
+            state[key] = _.union([state[key]], values);
+          }
+        } else {
+          if (values.length > 1) {
+            // This is an array encoded by commas.
+            state[key] = values;
+          }
+          else {
+            // This isn't an array at all.
+            state[key] = values[0];
+          }
+        }
+      });
+
+      hasStateBeenConstructedFromTheUrl = true;
+    }
 
     let query = [];
     let mergedState = _.extend(state, newState);
@@ -64,7 +99,8 @@ urlHistory = (function() {
   }
 
   const emptyState = function () {
-    state = {}
+    state = {};
+    hasStateBeenConstructedFromTheUrl = false;
   };
 
   return {
