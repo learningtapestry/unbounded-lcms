52d3608a565933a39e6663976f64fb5e6d6413b9
shlag3n <Oksana.Melnikova@gmail.com>
2016-07-05 22:01:30 +0300

    (#291, #306) fixed hscroll on cg (except tables) + added images to leadership

--- app/assets/javascripts/initializers/leadership.js.jsx
+++ app/assets/javascripts/initializers/leadership.js.jsx
@@ -16,6 +16,11 @@ $(function () {
   window.initializeLeadership = function() {
     if (!$('.c-ls-hero').length) return;
     initSwipers();
+    // $('.c-ls-slides__overlay').on('click', (e) => {
+    //   console.log('click');
+    //   $(".c-ls-slides--a__slide.swiper-slide-active iframe")[0].src += "?autoplay=1";
+    //   e.preventDefault();
+    // });
   }
 
 })
