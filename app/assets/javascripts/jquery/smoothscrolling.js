c8c8484595d143fdcf45f8f3b34c81d453c3db4b
RM Saksida <rm@saksida.com.br>
2016-07-06 14:52:56 -0300

    Fix JavaScript syntax
    File is using ES6 syntax without .jsx extension

--- app/assets/javascripts/jquery/smoothscrolling.js
+++ app/assets/javascripts/jquery/smoothscrolling.js
@@ -1,6 +1,6 @@
 (function($) {
 
-  $.fn.smoothscrolling = function(fnScrollingState=null) {
+  $.fn.smoothscrolling = function(fnScrollingState) {
     this.find('a[href^="#"], div[href^="#"]').on('click', function(event) {
       var target = $(event.target.attributes.href ? event.target.attributes.href.value : event.target.parentElement.attributes.href.value);
       if( target.length ) {
@@ -8,7 +8,7 @@
         if (fnScrollingState) fnScrollingState(true);
         $('html, body').animate({ scrollTop: target.offset().top }, 500)
                        .promise()
-                       .always(() => { if (fnScrollingState) fnScrollingState(false); });
+                       .always(function() { if (fnScrollingState) fnScrollingState(false); });
       }
     });
     return this;
