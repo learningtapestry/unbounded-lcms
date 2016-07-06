432f82a0a8e1a60b6941371cb43c272676b3d492
RM Saksida <rm@saksida.com.br>
2016-07-06 09:34:09 -0300

    Add Content-Disposition to CG PDF uploads

--- app/uploaders/content_guide_pdf_uploader.rb
+++ app/uploaders/content_guide_pdf_uploader.rb
@@ -4,4 +4,8 @@ class ContentGuidePdfUploader < CarrierWave::Uploader::Base
   def store_dir
     "content_guides/#{model.id}"
   end
+
+  def fog_attributes
+    { 'Content-Disposition' => 'attachment' }
+  end
 end
