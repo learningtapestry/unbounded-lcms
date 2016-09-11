2ee0e5df2e0d3c1687c3470786c48cdd0e253316
Alexander Zagaynov <zalex80@gmail.com>
2016-09-12 02:56:48 +0300

    make CW gem work locally in dev env without AWS creds

--- app/uploaders/content_guide_image_uploader.rb
+++ app/uploaders/content_guide_image_uploader.rb
@@ -1,6 +1,4 @@
 class ContentGuideImageUploader < CarrierWave::Uploader::Base
-  storage :fog
-
   def store_dir
     "content_guide_images/#{model.id}"
   end
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,10 +1,15 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_REGION']
-  }
-  config.fog_directory = ENV['AWS_S3_BUCKET_NAME']
-  config.fog_public = true
+  if Rails.env.development? && ENV['AWS_ACCESS_KEY_ID'].blank?
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION']
+    }
+    config.fog_directory = ENV['AWS_S3_BUCKET_NAME']
+    config.fog_public = true
+  end
 end
