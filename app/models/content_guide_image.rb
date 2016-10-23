db2b71d37cb86594f332777126e7560f77a0c744
Alex Nizamov <nizamov.alex@gmail.com>
2016-10-23 21:12:03 +0800

    Be aware of broken image when importing CGs (#420)

--- app/models/content_guide_image.rb
+++ app/models/content_guide_image.rb
@@ -1,3 +1,8 @@
 class ContentGuideImage < ActiveRecord::Base
   mount_uploader :file, ContentGuideImageUploader
+
+  def self.find_or_create_by_original_url(value)
+    create_with(remote_file_url: value).find_or_create_by!(original_url: value)
+  rescue ActiveRecord::RecordInvalid
+  end
 end
