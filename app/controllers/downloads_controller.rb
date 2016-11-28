5307c87a415866edc75b52ce7a6f8df793a9a863
shlag3n <oksana.melnikova@gmail.com>
2016-11-28 23:15:00 +0300

    (#434) added checking file existence for pdf preview

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -7,9 +7,12 @@ class DownloadsController < ApplicationController
   end
 
   def preview
+    RestClient.head(@download.attachment_url)
     s3_path = URI.parse(@download.attachment_url).path
     @pdf_url = "#{request.base_url}/pdf_proxy#{s3_path}"
     @pdf_filename = File.basename(s3_path)
+  rescue RestClient::ExceptionWithResponse => e
+    render text: e.response, status: '404'
   end
 
   private
