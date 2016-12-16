2164ff937a31f166d102ab3bc3975333ea2794cd
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-12-16 11:39:09 -0200

    Fix content-disposition and add index to is_language_progression

--- app/uploaders/language_progression_file_uploader.rb
+++ app/uploaders/language_progression_file_uploader.rb
@@ -2,4 +2,8 @@ class LanguageProgressionFileUploader < CarrierWave::Uploader::Base
   def store_dir
     "language_progressions/#{mounted_as}/#{model.id}"
   end
+
+  def fog_attributes
+    { 'Content-Disposition' => 'attachment' }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213140103) do
+ActiveRecord::Schema.define(version: 20161216133644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -358,6 +358,7 @@ ActiveRecord::Schema.define(version: 20161213140103) do
   add_index "standards", ["cluster_id"], name: "index_standards_on_cluster_id", using: :btree
   add_index "standards", ["domain_id"], name: "index_standards_on_domain_id", using: :btree
   add_index "standards", ["emphasis"], name: "index_standards_on_emphasis", using: :btree
+  add_index "standards", ["is_language_progression_standard"], name: "index_standards_on_is_language_progression_standard", using: :btree
   add_index "standards", ["name"], name: "index_standards_on_name", using: :btree
   add_index "standards", ["standard_strand_id"], name: "index_standards_on_standard_strand_id", using: :btree
   add_index "standards", ["subject"], name: "index_standards_on_subject", using: :btree
