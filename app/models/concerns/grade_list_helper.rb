848df9a0f1e8eb9b6c8699b0ad65245b6edbe651
shlag3n <oksana.melnikova@gmail.com>
2016-12-17 01:09:43 +0300

    (#463) styled color per avg grade for ei, search, preview

---
+++ app/models/concerns/grade_list_helper.rb
@@ -0,0 +1,35 @@
+module GradeListHelper
+  extend ActiveSupport::Concern
+  GRADES = ['prekindergarten', 'kindergarten', 'grade 1', 'grade 2', 'grade 3',
+            'grade 4', 'grade 5', 'grade 6', 'grade 7', 'grade 8', 'grade 9',
+            'grade 10', 'grade 11', 'grade 12'].freeze
+
+  included do
+    def grade_avg
+      grade_list_f = grade_list & GRADES
+      avg = grade_list_f.map { |g| GRADES.index(g) }
+                        .sum / grade_list_f.size
+      grade_abbr(GRADES[avg]) || 'base'
+    end
+
+    def grade_color_code
+      grade_list.map { |g| grade_abbr(g) }
+                .compact
+                .try(:first) || 'base'
+    end
+
+    def color_code(g = 'base')
+      subject_color_code = try(:subject) || 'default'
+      "#{subject_color_code}-#{g}"
+    end
+
+    protected
+
+    def grade_abbr(g)
+      grade = g.downcase
+      return 'k' if grade == 'kindergarten'
+      return 'pk' if grade == 'prekindergarten'
+      grade[/\d+/] if grade[/\d+/]
+    end
+  end
+end
