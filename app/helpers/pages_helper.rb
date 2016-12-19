de3ebc78fd40631d99c7a8b4a619018c60b053a7
shlag3n <oksana.melnikova@gmail.com>
2016-12-20 00:10:08 +0300

    (#462) minor styling changes

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,6 +4,6 @@ module PagesHelper
   def standardsinstitutes_link
     @standardsinstitutes_link ||= link_to('Register Now',
       'http://www.standardsinstitutes.org/institute/winter-2017-standards-institute',
-      class: 'o-btn o-btn--register u-margin-bottom--zero', target: '_blank')
+      class: 'o-btn o-btn--register o-btn--xs-full u-margin-bottom--zero', target: '_blank')
   end
 end
