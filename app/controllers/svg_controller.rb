e6933bbf630d6af663cb23f09803af2431de4675
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2017-01-10 15:17:32 -0200

    Adjust params to render thumbs for pinterest

--- app/controllers/svg_controller.rb
+++ app/controllers/svg_controller.rb
@@ -1,6 +1,8 @@
 class SVGController < ApplicationController
   def show
     @resource = Resource.find params[:resource_id]
-    render html: GenerateSVGThumbnailService.new(@resource).run.html_safe
+    media = params[:media] || :all
+    svg = GenerateSVGThumbnailService.new(@resource, media: media.to_sym).run
+    render html: svg.html_safe
   end
 end
