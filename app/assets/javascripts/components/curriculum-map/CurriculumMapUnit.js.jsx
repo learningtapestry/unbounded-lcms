3b9f8cd5af9466c9c7bb26bba390d76d687d306c
shlag3n <oksana.melnikova@gmail.com>
2017-01-13 18:49:08 +0300

    (#495) changed to show units for module curriculum map

--- app/assets/javascripts/components/curriculum-map/CurriculumMapUnit.js.jsx
+++ app/assets/javascripts/components/curriculum-map/CurriculumMapUnit.js.jsx
@@ -1,6 +1,6 @@
 function CurriculumMapUnit(props) {
   const curriculum = props.curriculum;
-  const isActive = _.includes(props.active, curriculum.id);
+  const isActive = _.includes(props.active, curriculum.id) || props.isActiveBranch;
   const cssClasses = classNames( 'o-c-map__unit-title',
                                { 'cs-txt-link--base': !isActive,
                                 [`cs-txt-link--${props.colorCode}`]: isActive });
@@ -9,7 +9,7 @@ function CurriculumMapUnit(props) {
                                    curriculum={lesson}
                                    colorCode={props.colorCode}
                                    active={props.active}
-                                   isUnitActive={isActive}
+                                   isActiveBranch={_.first(props.active) == curriculum.id || props.isActiveBranch}
                                    handlePopupState={props.handlePopupState} />
   );
   return (
