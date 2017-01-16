a9bdabb2a880765648ca9b1a14c429e717cafa54
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2017-01-16 16:44:53 -0200

    Change batik install path

--- .cloud66/install_batik.sh
+++ .cloud66/install_batik.sh
@@ -3,4 +3,4 @@ apt-get install libbatik-java -y
 wget http://www-us.apache.org/dist/xmlgraphics/batik/binaries/batik-bin-1.8.tar.gz
 tar xzvf batik-bin-1.8.tar.gz
 rm batik-bin-1.8.tar.gz
-mv batik-1.8/ $STACK_PATH/bin/batik
+mv batik-1.8/ /usr/local/bin/batik
