147a1c247fbb1c4fd4af1e536683dd7dd1c72284
shlag3n <oksana.melnikova@gmail.com>
2017-01-23 18:34:23 +0300

    changed button label for SI block main page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,7 +2,7 @@
 
 module PagesHelper
   def standardsinstitutes_link
-    @standardsinstitutes_link ||= link_to('Register Now',
+    @standardsinstitutes_link ||= link_to(t('ui.learn_more'),
       'http://www.standardsinstitutes.org/institute/winter-2017-standards-institute',
       class: 'o-btn o-btn--register o-btn--xs-full u-margin-bottom--zero', target: '_blank')
   end
