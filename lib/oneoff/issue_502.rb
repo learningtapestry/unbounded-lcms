8fb6ba3f10c35b53db220d021747fc669817a8d2
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2017-01-30 12:38:36 -0200

    oneoff::issue502 resource handler + log actions

--- lib/oneoff/issue_502.rb
+++ lib/oneoff/issue_502.rb
@@ -6,16 +6,41 @@ module Oneoff
     end
 
     def run
+      puts "resource_id,breadcrumbs,resource_title,action"
       CSV.foreach(csv_filepath, headers: true) do |row|
         context = build_context(row)
         next if context.nil?
 
-        curriculum = find(context)
-        puts curriculum.curriculum_type.name, curriculum.resource.title
+        ResourceHandler.new(context).run
       end
     end
 
-    def find(ctx)
+    def build_context(row)
+      title = row['Title']
+      return nil if title.nil?
+
+      ctx = {
+        row:    row,
+        grade:  title.match(/Grade (\d+) ELA/)[1],
+        unit:   title.match(/, Unit (\w+)/).try(:[], 1),
+        lesson: title.match(/, Part (\w+)$/).try(:[], 1)
+      }
+
+      ctx[:curriculum] = find_curriculum(ctx)
+      ctx[:resource] = ctx[:curriculum].resource
+
+      ctx[:level] = if ctx[:unit].nil? && ctx[:lesson].nil?
+                      :module
+                    elsif ctx[:unit].present? && ctx[:lesson].nil?
+                      :unit
+                    elsif ctx[:lesson].present?
+                      :lesson
+                    end
+
+      ctx
+    end
+
+    def find_curriculum(ctx)
       # Grade
       curr = Curriculum.trees.ela.with_resources.where_grade("grade #{ctx[:grade]}").first
 
@@ -38,25 +63,55 @@ module Oneoff
       curr
     end
 
-    def build_context(row)
-      title = row['Hierarchy Title']
-      return nil if title.nil?
+    private
 
-      ctx = {}
-      ctx[:grade]  = title.match(/Grade (\d+) ELA/)[1]
-      ctx[:unit]   = title.match(/, Unit (\w+)/).try(:[], 1)
-      ctx[:lesson] = title.match(/, Part (\w+)$/).try(:[], 1)
+      class ResourceHandler
+        attr_reader :context
 
-      ctx
-    end
+        def initialize(context)
+          @context = context
+        end
 
-    def module_level_tasks
-    end
+        def run
+          case context[:level]
+          when :module then module_level_tasks
+          when :unit   then unit_level_tasks
+          when :lesson then lesson_level_tasks
+          end
+        end
 
-    def unit_level_tasks
-    end
+        def resource
+          context[:resource]
+        end
 
-    def lesson_level_task
-    end
+        def module_level_tasks
+          # DO NOTHING
+        end
+
+        def unit_level_tasks
+          remove_all_downloads
+          update_description
+        end
+
+        def lesson_level_tasks
+          update_description
+        end
+
+        def remove_all_downloads
+          # ResourceDownload.where(resource_id: resource.id).delete_all if resource
+          if resource.downloads.count > 0
+            puts "#{resource.id},#{context[:curriculum].breadcrumb_title},resource.title,remove downloads"
+          end
+        end
+
+        def update_description
+          description = context[:row]['Description']
+          # resource.description = context[:row]['Description']
+          # resource.save
+          if description.present?
+            puts "#{resource.id},#{context[:curriculum].breadcrumb_title},resource.title,update description"
+          end
+        end
+      end
   end
 end
