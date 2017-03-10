317a2e25040032c3a97ef1bd7976e08f9130b1d2
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2017-03-10 15:09:46 -0300

    Chnage social sharing preview to use teaser on quick_ref_guides

--- app/presenters/social_media_presenter.rb
+++ app/presenters/social_media_presenter.rb
@@ -17,10 +17,10 @@ class SocialMediaPresenter
       "These guides are designed to explain what new, high standards for #{subject} "\
       "say about what students should learn in each grade, and what they mean for "\
       "curriculum and instruction"
-    elsif quick_reference_guide?
-      subject = target.subject == 'ela'? 'ELA' : 'Mathematics'
-      "This QRD outlines Elements of Aligned #{subject} Instruction and demonstrates "\
-      "relationships between the elements to assist educators."
+    # elsif quick_reference_guide?
+    #   subject = target.subject == 'ela'? 'ELA' : 'Mathematics'
+    #   "This QRD outlines Elements of Aligned #{subject} Instruction and demonstrates "\
+    #   "relationships between the elements to assist educators."
     else
       target.try(:teaser).try(:html_safe)
     end
