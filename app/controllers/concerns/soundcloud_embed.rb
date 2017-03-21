36f544e8ebb2994b72adfcbc95a8cdc3292dca11
shlag3n <oksana.melnikova@gmail.com>
2017-03-21 18:24:54 +0300

    fixed soundcloud embed url

--- app/controllers/concerns/soundcloud_embed.rb
+++ app/controllers/concerns/soundcloud_embed.rb
@@ -6,7 +6,7 @@ module SoundcloudEmbed
 
   def soundcloud_embed(url, subject)
     color = SUBJECT_COLORS[subject || :default]
-    oembed_url = "http://soundcloud.com/oembed?url=#{url}iframe=true&maxheight=166&color=#{color}&auto_play=false&format=json"
+    oembed_url = "http://soundcloud.com/oembed?url=#{url}&iframe=true&maxheight=166&color=#{color}&auto_play=false&format=json"
     RestClient.get(oembed_url) do |response|
       if response.code == 200
         oembed = JSON.parse(response)['html']
