db808f726f59eccb8c3270100c946748798284e5
RM Saksida <rm@saksida.com.br>
2017-03-29 13:13:22 -0300

    Update Standards Institutes link and date

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,7 +3,7 @@
 module PagesHelper
   def standardsinstitutes_link
     @standardsinstitutes_link ||= link_to(t('ui.learn_more'),
-      'http://www.standardsinstitutes.org/institute/winter-2017-standards-institute',
+      'http://www.standardsinstitutes.org/',
       class: 'o-btn o-btn--register o-btn--xs-full u-margin-bottom--zero', target: '_blank')
   end
 end
