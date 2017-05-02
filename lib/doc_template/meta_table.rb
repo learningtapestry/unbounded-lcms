5ab3110f2644af37ba5de74343d0b8885b52e096
Antonio Lorusso <antonio.lorusso@gmail.com>
2017-05-02 13:41:07 +0200

    metadata table parser

--- lib/doc_template/meta_table.rb
+++ lib/doc_template/meta_table.rb
@@ -1,19 +1,24 @@
 module DocTemplate
   class MetaTable
+    HEADER_LABEL = 'document-metadata'
+
     def self.parse(fragment)
       table = new
       table.parse(fragment)
     end
 
     def parse(fragment)
-      binding.pry
       table = fragment.at_css('table')
       return self unless table
 
       table.search('br').each { |br| br.replace("\n") }
-      @data = table.css('tr').map do |tr|
+      data_collection = table.css('tr').map do |tr|
         key, value = tr.css('td').map(&:content).map(&:strip)
-      end
+        next if key == HEADER_LABEL
+        [key, value]
+      end.compact
+
+      @data = data_collection.to_h
 
       table.remove
       self
