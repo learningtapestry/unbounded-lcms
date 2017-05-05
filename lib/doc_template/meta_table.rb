a099b0e7082145de861c1bbbb7adf1d7e6acff2f
Alexander Kuznetsov <paranoic.san@gmail.com>
2017-05-05 12:21:24 +0200

    Inserts `Teachers Guidance` for ELA G6
    Also refactors metadata parsing to store in DB downcased keys values

--- lib/doc_template/meta_table.rb
+++ lib/doc_template/meta_table.rb
@@ -11,20 +11,20 @@ module DocTemplate
       table = fragment.at_xpath("table[.//*[contains(text(), '#{HEADER_LABEL}')]]")
       return self unless table
 
-      table.at_css('tr > td').remove
-
       table.search('br').each { |br| br.replace("\n") }
-      data_collection = table.css('tr').map do |tr|
-        tr.css('td').map(&:content).map(&:strip)
-      end.compact
-
-      @data = data_collection.select(&:present?).to_h
+      @data = {}.tap do |result|
+                table.xpath('.//tr[position() > 1]').each do |tr|
+                  key = tr.at_xpath('./td[1]').text.strip.downcase
+                  next if key.blank?
+                  result[key] = tr.at_xpath('./td[2]').text.strip
+                end
+              end
 
       table.remove
       self
     end
 
-    def render
+    def data
       @data || {}
     end
   end
