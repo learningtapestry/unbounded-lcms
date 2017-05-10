29b5bd366587dd87cac705e586391fe29dc320d4
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2017-05-09 23:32:18 -0300

    add http basic auth for the pilot prod server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # require auth for acessing the pilot
+  http_basic_authenticate_with name: ENV['HTTP_AUTH_NAME'], password: ENV['HTTP_AUTH_PASS'] if Rails.env.production?
+
   rescue_from ActiveRecord::RecordNotFound do
     render 'pages/not_found', status: :not_found
   end
