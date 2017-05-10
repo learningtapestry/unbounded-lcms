1c153117af2dfa048629394e22a42cc76ffb6c7f
shlag3n <oksana.melnikova@gmail.com>
2017-05-10 17:26:18 +0300

    (#61, #116) styled&added lesson header, line break for h2
    solved some naming issues, added styles for h2/h3

--- app/controllers/lesson_documents_controller.rb
+++ app/controllers/lesson_documents_controller.rb
@@ -1,6 +1,11 @@
 class LessonDocumentsController < ApplicationController
+  include CurriculumMapProps
+
   def show
-    @lesson_document = LessonDocument.find(params[:id])
+    @lesson_document = LessonDocumentPresenter.new LessonDocument.find(params[:id])
+    @curriculum = @lesson_document.resource.try(:first_tree)
+    # set props to build Curriculum Map
+    set_index_props
     respond_to do |format|
       format.html
     end
---
+++ app/presenters/lesson_document_presenter.rb
@@ -0,0 +1,66 @@
+class LessonDocumentPresenter < BasePresenter
+  TOPIC_FULL   = { 'ela' => 'Unit', 'math' => 'Topic' }.freeze
+  SUBJECT_FULL = { 'ela' => 'ELA', 'math' => 'Math' }.freeze
+
+  def ld_metadata
+    @ld_metadata ||= DocTemplate::Objects::BaseMetadata.build_from(metadata)
+  end
+
+  def color_code
+    "#{subject}-base"
+  end
+
+  def full_breadcrumb
+    [
+      SUBJECT_FULL[subject] || subject,
+      grade.to_i.zero? ? grade : "Grade #{grade}",
+      ll_strand? ? ld_module : "Module #{ld_module.try(:upcase)}",
+      "#{TOPIC_FULL[subject]} #{topic.try(:upcase)}",
+      "Lesson #{ld_metadata.lesson}"
+    ].join(' / ')
+  end
+
+  def short_title
+    "Lesson #{ld_metadata.lesson}"
+  end
+
+  def standards
+    ld_metadata.standard || ld_metadata.lesson_standard
+  end
+
+  def math_practice
+    ld_metadata.lesson_mathematical_practice.squish
+  end
+
+  def title
+    ld_metadata.title
+  end
+
+  def teaser
+    ld_metadata.teaser
+  end
+
+  def description
+    ld_metadata.lesson_objective || ld_metadata.description
+  end
+
+  def subject
+    ld_metadata.resource_subject
+  end
+
+  def grade
+    ld_metadata.grade[/\d+/] || ld_metadata.grade
+  end
+
+  def ld_module
+    ela? ? ld_metadata.module : ld_metadata.unit
+  end
+
+  def topic
+    ela? ? ld_metadata.unit : ld_metadata.topic
+  end
+
+  def ll_strand?
+    ld_metadata.module =~ /strand/i
+  end
+end
