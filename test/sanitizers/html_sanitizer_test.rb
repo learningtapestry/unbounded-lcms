9a751f42a55f2063cd34d1eb16067730f8037185
antonio lorusso <zekus.one@gmail.com>
2017-05-11 03:26:57 +0200

    fixes image addresses for google charts (#133)

--- test/sanitizers/html_sanitizer_test.rb
+++ test/sanitizers/html_sanitizer_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class HtmlSanitizerTest < ActiveSupport::TestCase
   def html
-    <<-TABLE
+    <<-table
     <tbody>
       <tr>
         <td colspan=\"2\" rowspan=\"1\">
@@ -12,11 +12,28 @@ class HtmlSanitizerTest < ActiveSupport::TestCase
         </td>
       </tr>
     </tbody>
-    TABLE
+    table
   end
 
   def test_remove_gdocs_suggestion
     sanitized = HtmlSanitizer.sanitize(html)
     assert sanitized.include?('cmnt_ref') == false
+  end
+
+  def charts_html
+    <<-html
+    <p>
+      <span>Using the floor tiles design shown below, create </span>
+      <img src="https://www.google.com/chart?cht=tx&amp;chf=bg,s,FFFFFF00&amp;chco=000000&amp;chl=4">
+      <span>&nbsp;different ratios related to the image. &nbsp;Describe the ratio relationship, and write the ratio in the form </span>
+      <img src="https://www.google.com/chart?cht=tx&amp;chf=bg,s,FFFFFF00&amp;chco=000000&amp;chl=A%3AB" data-pin-nopin="true"><span>&nbsp;or the form </span><img src="https://www.google.com/chart?cht=tx&amp;chf=bg,s,FFFFFF00&amp;chco=000000&amp;chl=A"><span>&nbsp;to </span><img src="https://www.google.com/chart?cht=tx&amp;chf=bg,s,FFFFFF00&amp;chco=000000&amp;chl=B"><span>. </span>
+    </p>
+    html
+  end
+
+  def test_replace_gdoc_charts
+    sanitized = HtmlSanitizer.sanitize(charts_html)
+    assert sanitized.include?('www.google') == false
+    assert sanitized.include?('chart.googleapis') == true
   end
 end
