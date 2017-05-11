93e2934cf39e8f6ac2bafcf3d232226068d0fcb2
Alexander Kuznetsov <paranoic.san@gmail.com>
2017-05-11 08:19:47 +0200

    [Tags] ELA - Word Phrase Definition (May 15th)

---
+++ lib/doc_template/tags/def_tag.rb
@@ -0,0 +1,27 @@
+module DocTemplate
+  class DefTag < Tag
+    include ERB::Util
+
+    TAG_NAME = 'def'.freeze
+    TAG_SEPARATOR = '[separator]'.freeze
+    TEMPLATE = 'def.html.erb'
+
+    def parse(node, opts = {})
+      # preserving text around tag
+      @data = {}
+      if (data = node.content.sub(/\[[^\]]*\]/, TAG_SEPARATOR).split(TAG_SEPARATOR, 2))
+        @data[:append] = data[1]
+        @data[:prepend] = data[0]
+      end
+
+      @definition, @description = opts[:value].split(';').map(&:strip)
+
+      template = File.read template_path(TEMPLATE)
+      node.replace ERB.new(template).result(binding)
+      @result = node
+      self
+    end
+  end
+
+  Template.register_tag(DefTag::TAG_NAME, DefTag)
+end
