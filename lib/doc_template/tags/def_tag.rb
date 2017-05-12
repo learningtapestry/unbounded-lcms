dd52a1fa2fc0f205672d2f9604f956c12fc64165
Alexander Kuznetsov <paranoic.san@gmail.com>
2017-05-12 13:45:25 +0200

    Fixes missed declaration

--- lib/doc_template/tags/def_tag.rb
+++ lib/doc_template/tags/def_tag.rb
@@ -7,13 +7,14 @@ module DocTemplate
       TAG_SEPARATOR = '[separator]'.freeze
       TEMPLATE = 'def.html.erb'.freeze
 
-    def parse(node, opts = {})
-      # preserving text around tag
-      # TODO: Extract to the parent class@data = {}
-      if (data = node.content.sub(/\[[^\]]*\]/, TAG_SEPARATOR).split(TAG_SEPARATOR, 2))
-        @data[:append] = data[1]
-        @data[:prepend] = data[0]
-      end
+      def parse(node, opts = {})
+        # preserving text around tag
+        # TODO: Extract to the parent class@data = {}
+        @data = {}
+        if (data = node.content.sub(/\[[^\]]*\]/, TAG_SEPARATOR).split(TAG_SEPARATOR, 2))
+          @data[:append] = data[1]
+          @data[:prepend] = data[0]
+        end
 
         @definition, @description = opts[:value].split(';').map(&:strip)
 
