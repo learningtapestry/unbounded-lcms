d756fa83a2bda24960355e77bff4777aa9eed7d9
shlag3n <oksana.melnikova@gmail.com>
2017-05-13 10:15:11 +0300

    (#105) refactored optbreak to handle all cases of tag placement

--- lib/doc_template/tags/group_tag.rb
+++ lib/doc_template/tags/group_tag.rb
@@ -2,7 +2,6 @@ module DocTemplate
   module Tags
     class GroupTag < BaseTag
       TAG_NAME  = 'group'.freeze
-      OPT_BREAK = '[optbreak]'.freeze
       TEMPLATE  = 'h2-header.html.erb'.freeze
 
       def parse(node, opts = {})
@@ -17,20 +16,10 @@ module DocTemplate
 
       def parse_ela6(node, group, opts)
         table = node.ancestors('table')
-        # check if there is OptBreak before group
-        opts[:agenda].add_break(group) if opt_break?(node)
         @result = table.before(parse_template(group, TEMPLATE))
         # remove table if it was last group without sections in a table
         node.ancestors('tr').first.next_element ? node.remove : table.remove
         self
-      end
-
-      def opt_break?(node)
-        prev = node.ancestors('tr').first.previous_element
-        return false unless prev.present?
-        result = prev.inner_html =~ /#{Regexp.escape(OPT_BREAK)}/
-        prev.remove if result
-        result
       end
     end
   end
