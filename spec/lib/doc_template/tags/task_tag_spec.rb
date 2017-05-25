83891823c2130356d12a7da1c7ebe5c62baf18d1
Alexander Kuznetsov <paranoic.san@gmail.com>
2017-05-25 08:19:41 +0200

    [Tags] MATH - Task Iteration #107 (#185)
    When `activity-metadata` tables are being parsed all task tags are transformed to iterate the task number. After that `TaskTag` handles transformed tags as usually.

---
+++ spec/lib/doc_template/tags/task_tag_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe DocTemplate::Tags::TaskTag do
+  let(:node) { Nokogiri::HTML(original_content).at_xpath('*//p') }
+  let(:original_content) { "<p><span>[task: #{task_number}]</span></p><p><span>" }
+  let(:tag) { described_class.new }
+  let(:task_number) { 2 }
+
+  subject { tag.parse(node, value: task_number).render.to_html }
+
+  it 'substitutes the tag' do
+    expect(subject).to_not include '[task: '
+    expect(subject).to include "<h4>Task #{task_number}</h4>"
+  end
+end
