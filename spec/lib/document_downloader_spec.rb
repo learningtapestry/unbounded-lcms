c9f57783ad06d28c45580da4d516a0bf1cf5b882
Alexander Kuznetsov <paranoic.san@gmail.com>
2017-05-25 08:20:06 +0200

    Handles Google Drawings objects in the documents #186 (#189)
    When source document contains Google Drawings we fetch such objects and encode them with Base64

--- spec/lib/document_downloader_spec.rb
+++ spec/lib/document_downloader_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 describe DocumentDownloader::GDoc do
-  let(:downloader) { described_class.new nil, file_url, target_class }
+  let(:access_token) { 'token' }
+  let(:credentials) { double access_token: access_token }
+  let(:downloader) { described_class.new credentials, file_url, target_class }
   let(:file_id) { '16sDahoxlTIoGwp8SrtrGDao98GQuZMQnoc-7PKOUxUM' }
   let(:file_url) { "https://docs.google.com/document/d/#{file_id}/edit" }
   let(:target_class) { LessonDocument }
@@ -39,6 +41,42 @@ describe DocumentDownloader::GDoc do
 
     it 'returns it' do
       expect(subject).to be_a target_class
+    end
+
+    context 'when document contains Google Drawings objects' do
+      let(:document) { LessonDocument.last }
+      let(:drawing_encoded) { 'body' }
+      let(:drawing_url) { 'https://docs.google.com/drawings/image?id=s_uiJ2KNBacy7Mt2DqQn5aQ&amp;rev=1&amp;h=125&amp;w=345&amp;ac=1' }
+      let(:drawing_url_clear) { CGI.unescapeHTML drawing_url }
+      let(:file_content) { %( <p><span><img alt="" src="#{drawing_url}" title=""></span></p> ) }
+      let(:headers) { { 'Authorization' => "Bearer #{access_token}" } }
+      let(:response) { OpenStruct.new content_type: 'image/png' }
+      let(:service) { double export_file: file_content }
+
+      before do
+        allow_any_instance_of(described_class).to receive(:content).and_call_original
+        allow_any_instance_of(described_class).to receive(:service).and_return(service)
+
+        allow(HTTParty).to receive(:get).with(drawing_url_clear, headers: headers).and_return(response)
+        allow(Base64).to receive(:encode64).with(response).and_return(drawing_encoded)
+      end
+
+      it 'fetches them' do
+        expect(HTTParty).to receive(:get).with(drawing_url_clear, headers: headers)
+        subject
+      end
+
+      it 'encodes them using Base64' do
+        expect(Base64).to receive(:encode64).with(response)
+        subject
+      end
+
+      it 'substitutes the original URL' do
+        subject
+        expect(document.original_content).to_not include(drawing_url)
+        expect(document.original_content).to_not include(drawing_url_clear)
+        expect(document.original_content).to include(drawing_encoded)
+      end
     end
   end
 end
