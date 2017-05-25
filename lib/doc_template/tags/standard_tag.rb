355606bbf89ce0b44803ef883d34599056633016
Alexander Kuznetsov <paranoic.san@gmail.com>
2017-05-25 08:19:04 +0200

    [Tag] Standards - both ELA & MATH #181 (#183)
    Changes the logic stadards data is fetched and displayed.

--- lib/doc_template/tags/standard_tag.rb
+++ lib/doc_template/tags/standard_tag.rb
@@ -3,68 +3,8 @@ module DocTemplate
     class StandardTag < BaseTag
       include ERB::Util
 
-      TAG_DATA = {
-        l: [
-          { definition: 'Language', description: 'Conventions of Standard English' },
-          { definition: 'Language', description: 'Conventions of Standard English' },
-          { definition: 'Language', description: 'Knowledge of Language' },
-          { definition: 'Language', description: 'Vocabulary Acquisition and Use' },
-          { definition: 'Language', description: 'Vocabulary Acquisition and Use' },
-          { definition: 'Language', description: 'Vocabulary Acquisition and Use' }
-        ],
-        rf: [
-          { definition: 'Reading: Foundational Skills', description: 'Print Concepts' },
-          { definition: 'Reading: Foundational Skills', description: 'Phonological Awareness' },
-          { definition: 'Reading: Foundational Skills', description: 'Phonics and Word Recognition' },
-          { definition: 'Reading: Foundational Skills', description: 'Fluency' }
-        ],
-        ri: [
-          { definition: 'Reading: Informational Text', description: 'Key Ideas and Details' },
-          { definition: 'Reading: Informational Text', description: 'Key Ideas and Details' },
-          { definition: 'Reading: Informational Text', description: 'Key Ideas and Details' },
-          { definition: 'Reading: Informational Text', description: 'Craft and Structure' },
-          { definition: 'Reading: Informational Text', description: 'Craft and Structure' },
-          { definition: 'Reading: Informational Text', description: 'Craft and Structure' },
-          { definition: 'Reading: Informational Text', description: 'Integration of Knowledge and Ideas' },
-          { definition: 'Reading: Informational Text', description: 'Integration of Knowledge and Ideas' },
-          { definition: 'Reading: Informational Text', description: 'Integration of Knowledge and Ideas' },
-          { definition: 'Reading: Informational Text', description: 'Range of Reading and Level of Text Complexity' }
-        ],
-        rl: [
-          { definition: 'Reading: Literature', description: 'Key Ideas and Details' },
-          { definition: 'Reading: Literature', description: 'Key Ideas and Details' },
-          { definition: 'Reading: Literature', description: 'Key Ideas and Details' },
-          { definition: 'Reading: Literature', description: 'Craft and Structure' },
-          { definition: 'Reading: Literature', description: 'Craft and Structure' },
-          { definition: 'Reading: Literature', description: 'Craft and Structure' },
-          { definition: 'Reading: Literature', description: 'Integration of Knowledge and Ideas' },
-          { definition: 'Reading: Literature', description: 'Integration of Knowledge and Ideas' },
-          { definition: 'Reading: Literature', description: 'Integration of Knowledge and Ideas' },
-          { definition: 'Reading: Literature', description: 'Range of Reading and Level of Text Complexity' }
-        ],
-        sl: [
-          { definition: 'Speaking & Listening', description: 'Comprehension and Collaboration' },
-          { definition: 'Speaking & Listening', description: 'Comprehension and Collaboration' },
-          { definition: 'Speaking & Listening', description: 'Comprehension and Collaboration' },
-          { definition: 'Speaking & Listening', description: 'Presentation of Knowledge and Ideas' },
-          { definition: 'Speaking & Listening', description: 'Presentation of Knowledge and Ideas' },
-          { definition: 'Speaking & Listening', description: 'Presentation of Knowledge and Ideas' }
-        ],
-        w: [
-          { definition: 'Writing', description: 'Text Types and Purposes' },
-          { definition: 'Writing', description: 'Text Types and Purposes' },
-          { definition: 'Writing', description: 'Text Types and Purposes' },
-          { definition: 'Writing', description: 'Production and Distribution of Writing' },
-          { definition: 'Writing', description: 'Production and Distribution of Writing' },
-          { definition: 'Writing', description: 'Production and Distribution of Writing' },
-          { definition: 'Writing', description: 'Research to Build and Present Knowledge' },
-          { definition: 'Writing', description: 'Research to Build and Present Knowledge' },
-          { definition: 'Writing', description: 'Research to Build and Present Knowledge' },
-          { definition: 'Writing', description: 'Range of Writing' }
-        ]
-      }.freeze
-      STANDARD_RE = /[^\[\]]*\[(ela\.)?(rl|ri|rf|w|sl|l)\.\d+\.(\d{1,2})\]/i
-      TAG_NAME = /^(ela\.)?(rl|ri|rf|w|sl|l)\.\d+\.\d{1,2}/ # RL.2.4 or ELA.RL.2.4
+      STANDARD_RE = /[^\[\]]*\[(ela\.)?((rl|ri|rf|w|sl|l)\.[^\]]+)\]/i # [rl.2.2a.2]
+      TAG_NAME = /(ela\.)?((rl|ri|rf|w|sl|l)\.[^\]]+)/ # RL.2.4 or ELA.RL.2.4
       TAG_RE = /\[[^\]]*\]/
       TAG_SEPARATOR = '[separator]'.freeze
       TEMPLATE = 'standard.html.erb'.freeze
@@ -104,23 +44,17 @@ module DocTemplate
         end
       end
 
-      def fetch_standard(text)
+      def fetch_description(text)
         return unless (matches = STANDARD_RE.match text)
 
-        standard_name = matches[2].downcase.to_sym
-        standard_num = matches[3].to_i - 1
-        TAG_DATA[standard_name].try(:[], standard_num)
+        name = matches[2].downcase.to_sym
+        Standard.search_by_name(name).first.try(:description)
       end
 
       def render_template(node)
         @data = fetch_data node.inner_html
-        @standard_shortcut = TAG_RE.match(node.content).try(:[], 0).try(:gsub, /\[|\]/, '')
-
-        standard_data = fetch_standard node.content
-        if standard_data.present?
-          @definition = standard_data[:definition]
-          @description = standard_data[:description]
-        end
+        @standard_shortcut = TAG_RE.match(node.content).try(:[], 0).try(:gsub, /[\[\]]/, '')
+        @description = fetch_description node.content
 
         template = File.read template_path(TEMPLATE)
         ERB.new(template).result(binding).gsub(/\s{2,}</, '<')
---
+++ spec/lib/doc_template/tags/stadard_tag_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe DocTemplate::Tags::StandardTag do
+  let(:original_content) do
+    <<-HTML
+      <ul class="lst-kix_nxend745gryj-0 start">
+        <li><span>Use words and phrases acquired through conversations, reading and being read to, and responding to the text </span><span>[</span><span>#{standard_name}</span><span>].</span></li>
+        <li><span>Prior to listening to </span><span>The Fisherman and His Wife</span><span>, orally predict which character has magical powers and then compare the actual outcome to the prediction</span><span>.</span></li>
+      </ul>
+    HTML
+  end
+  let(:standard_name) { 'L.2.6' }
+  let(:tag) { described_class.new }
+  let(:node) do
+    html = Nokogiri::HTML original_content
+    html.at_xpath('*//li') # it's the parent node of the one containing the tag itself
+  end
+
+  subject { tag.parse(node, {}).render.to_html }
+
+  it 'fetches DB for the description of a stadard' do
+    expect(Standard).to receive(:search_by_name).with(standard_name.downcase.to_sym).and_call_original
+    subject
+  end
+
+  it 'preserves <li> markup' do
+    expect(subject).to include '<li>'
+    expect(subject).to include '</li>'
+  end
+
+  it 'renders corresponding template' do
+    expect(ERB).to receive_message_chain(:new, :result).and_return('')
+    subject
+  end
+end
