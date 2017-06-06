726b1572e7dad9e743edc331668d442a02b9fb45
Alexander Kuznetsov <paranoic.san@gmail.com>
2017-06-06 16:04:43 +0200

    Simplifies `PDFDownloadable`

--- app/models/concerns/pdf_downloadable.rb
+++ app/models/concerns/pdf_downloadable.rb
@@ -6,19 +6,18 @@ module PDFDownloadable
     "#{base_title}_v#{version}"
   end
 
-  def pdf_version
-    v = pdf.url.split('_').last
-    v[1..-1].to_i if v.start_with?('v')
-  end
-
-  def pdf_refresh?
-    pdf.blank? || pdf_version != version
-  end
-
   # If PDF is stale, refresh it using a remote URL
   def pdf_refresh!(new_pdf_url)
-    return unless pdf_refresh?
+    return if pdf_version == version
     self.remote_pdf_url = new_pdf_url
     save!
+  end
+
+  private
+
+  def pdf_version
+    return if pdf.blank?
+    v = pdf.url.split('_').last
+    v[1..-1].to_i if v.start_with?('v')
   end
 end
