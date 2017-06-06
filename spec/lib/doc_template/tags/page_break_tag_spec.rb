b157f2261b266544f5378b48ff766b31c9b1fc9d
shlag3n <oksana.melnikova@gmail.com>
2017-06-06 23:37:49 +0300

    (#220) added page break tag

---
+++ spec/lib/doc_template/tags/page_break_tag_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe DocTemplate::Tags::PageBreakTag do
+  let(:node) { Nokogiri::HTML(original_content).at_xpath('*//p') }
+  let(:original_content) { '<p><span>[page-break]</span></p>' }
+  let(:tag) { described_class.new }
+  let(:cls) { 'u-pdf-alwaysbreak' }
+
+  subject { tag.parse(node, {}).render.to_html }
+
+  it 'substitutes the tag' do
+    expect(subject).to_not include '[page-break]'
+    expect(subject).to include "<div class=\"#{cls}\">"
+  end
+end
