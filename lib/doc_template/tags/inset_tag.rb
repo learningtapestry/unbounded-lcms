3f0a85bf68cbfc92c1e1c4b54bd862a8162e5051
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2017-06-09 13:14:35 -0300

    (#238) add tests +  changes according to code review

--- lib/doc_template/tags/inset_tag.rb
+++ lib/doc_template/tags/inset_tag.rb
@@ -2,29 +2,33 @@ module DocTemplate
   module Tags
     class InsetTag < BaseTag
       TAG_NAME = 'inset'.freeze
+      STYLES_REGEXP = {
+        bold: /font-weight:[6-9]00/i,
+        italic: /font-style:italic/i
+      }.freeze
 
       def parse(node, opts = {})
+        content = opts[:value] == 'end' ? '' : parse_content(node)
+        node = node.replace content
         @result = node
-
-        if opts[:value] != 'end'
-          # we have to collect all the next siblings until the we find the end tag
-          content = [].tap do |result|
-            while (sibling = node.next_sibling)
-              break if end?(sibling)
-
-              sibling = add_inset_class(sibling)
-              sibling = preserve_styles(sibling)
-              result << sibling.to_html
-              sibling.remove
-            end
-          end.join
-          node.replace content
-        end
-        remove_node
         self
       end
 
       private
+
+      def parse_content(node)
+        # we have to collect all the next siblings until the we find the end tag
+        [].tap do |result|
+          while (sibling = node.next_sibling)
+            break if end?(sibling)
+
+            sibling = add_inset_class(sibling)
+            sibling = preserve_styles(sibling)
+            result << sibling.to_html
+            sibling.remove
+          end
+        end.join
+      end
 
       def end?(node)
         node.content =~ /\[#{TAG_NAME}:end\]/
@@ -37,8 +41,8 @@ module DocTemplate
 
       def preserve_styles(node)
         node.children.each do |el|
-          el['class'] = el['class'].to_s + ' text-bold' if el['style'] =~ /font-weight:[6-9]00/i
-          el['class'] = el['class'].to_s + ' text-italic' if el['style'] =~ /font-style:italic/i
+          el['class'] = el['class'].to_s + ' text-bold' if el['style'] =~ STYLES_REGEXP[:bold]
+          el['class'] = el['class'].to_s + ' text-italic' if el['style'] =~ STYLES_REGEXP[:italic]
         end
         node
       end
