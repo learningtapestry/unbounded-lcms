47080c2ae0c7a16f8c8fbcc5ec39fd61360813f9
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2017-06-13 01:30:43 -0300

    (#221) add answer-space tag

---
+++ lib/doc_template/tags/answer_space_tag.rb
@@ -0,0 +1,25 @@
+module DocTemplate
+  module Tags
+    class AnswerSpaceTag < BaseTag
+      SPACE_SIZE = {
+        s: 5, # small = 5 lines
+        m: 10, # medium = 10 lines
+        l: 20, # large = 20 lines
+      }.freeze
+      TAG_NAME = 'answer-space'.freeze
+
+      def parse(node, opts = {})
+        num_of_lines = SPACE_SIZE[opts[:value].try(:to_sym)]
+        if num_of_lines
+          space = '<br>' * num_of_lines
+          content = node.to_html.sub(/\[#{self.class::TAG_NAME}:\s?[s|m|l]\]/i, space)
+          node = node.replace(content)
+        end
+        @result = node
+        self
+      end
+    end
+  end
+
+  Template.register_tag(Tags::AnswerSpaceTag::TAG_NAME, Tags::AnswerSpaceTag)
+end
