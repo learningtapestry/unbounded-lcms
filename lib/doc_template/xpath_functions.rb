3f0271ec54e54fec2604f97d68a5d49c8ace61e3
shlag3n <oksana.melnikova@gmail.com>
2017-06-15 00:02:57 +0300

    (#204) extended options for table header
    (#297) updated text

--- lib/doc_template/xpath_functions.rb
+++ lib/doc_template/xpath_functions.rb
@@ -7,5 +7,9 @@ module DocTemplate
     def case_insensitive_contains(node_set, str_to_match)
       node_set.find_all { |node| node.to_s.downcase.include?(str_to_match.to_s.downcase) }
     end
+
+    def case_regular(node_set, re_to_match)
+      node_set if node_set.to_s =~ Regexp.new(re_to_match, true)
+    end
   end
 end
