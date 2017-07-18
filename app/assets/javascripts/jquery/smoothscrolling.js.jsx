0ba613e5f9cd5abd5de744f0b17e1e720c685107
Alexander Kuznetsov <paranoic.san@gmail.com>
2017-07-18 08:42:10 +0200

    Fixes specs

--- app/assets/javascripts/jquery/smoothscrolling.js.jsx
+++ app/assets/javascripts/jquery/smoothscrolling.js.jsx
@@ -1,12 +1,11 @@
 (function($) {
-
   $.fn.smoothscrolling = function(fnScrollingState, topMarginSelector = null) {
     this.find('a[href^="#"], div[href^="#"]').on('click', function(event) {
-      var target = $(event.target.attributes.href ? event.target.attributes.href.value : event.target.parentElement.attributes.href.value);
+      const target = $(event.target.attributes.href ? event.target.attributes.href.value : event.target.parentElement.attributes.href.value);
       if( target.length ) {
         event.preventDefault();
         if (fnScrollingState) fnScrollingState(true);
-        var topMargin = topMarginSelector ? $(topMarginSelector).outerHeight(true) || 0 : 0;
+        const topMargin = topMarginSelector ? $(topMarginSelector).outerHeight(true) || 0 : 0;
         $('html, body').animate({ scrollTop: target.offset().top - topMargin }, 500)
                        .promise()
                        .always(function() { if (fnScrollingState) fnScrollingState(false); });
