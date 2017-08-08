a055d1369ed87206707923d4a31b04925c6a2176
Farid Bagishev <farid@bagishev.ru>
2017-08-08 13:37:27 +0500

    Reduce number of pages on the website

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    redirect_to action: 'index', controller: 'explore_curriculum'
+  end
 end
