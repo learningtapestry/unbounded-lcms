eb0fc250672887acce9176994ce5cc637179a3c5
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2017-08-14 02:46:18 -0300

    (#453) Fix search pagination + pull multi-step search from master

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 require 'elasticsearch/model'
+require "#{Rails.root}/lib/elasticsearch/persistence/repository/response/results"
 
 Elasticsearch::Model.client = Elasticsearch::Client.new(
   host: ENV['ELASTICSEARCH_ADDRESS']
 )
+Hashie.logger = Logger.new('/dev/null')
