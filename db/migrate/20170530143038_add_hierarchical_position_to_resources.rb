e57ad63bf7bdae8195723086fd6cf54560fe14c9
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2017-08-14 01:33:25 -0300

    Fixes for deploying to prod

--- db/migrate/20170530143038_add_hierarchical_position_to_resources.rb
+++ db/migrate/20170530143038_add_hierarchical_position_to_resources.rb
@@ -3,7 +3,7 @@ class AddHierarchicalPositionToResources < ActiveRecord::Migration
     add_column :resources, :hierarchical_position, :string, index: true
   end
 
-  def data
-    GenerateHierarchicalPositions.new.generate!
-  end
+  # def data
+  #   GenerateHierarchicalPositions.new.generate!
+  # end
 end
