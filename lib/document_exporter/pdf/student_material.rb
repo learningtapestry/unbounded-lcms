ba8692a331e162410961acb3c4bb5cf45d72bd74
Alexander Kuznetsov <paranoic.san@gmail.com>
2017-08-15 12:51:49 +0200

    (#481, #482) Order of materials in the end of the lesson (in PDF packages)

--- lib/document_exporter/pdf/student_material.rb
+++ lib/document_exporter/pdf/student_material.rb
@@ -7,13 +7,10 @@ module DocumentExporter
         pdf = CombinePDF.new
 
         scope = @document.materials.where_metadata_any_of(config_for(:student))
-
         scope = scope.where(id: included_materials) if @options[:excludes].present?
 
-        scope.each do |material|
-          url = @document.links['materials']&.dig(material.id.to_s, 'url')
-          pdf << CombinePDF.parse(Net::HTTP.get_response(URI.parse(url)).body)
-        end
+        material_ids = ordered_materials scope.pluck(:id)
+        pdf = conbine_pdf_for pdf, material_ids
         pdf.to_pdf
       end
     end
