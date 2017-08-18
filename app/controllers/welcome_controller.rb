5228488c5515c111c04ae8ab25bec01c7133dc24
Alexander Kuznetsov <paranoic.san@gmail.com>
2017-08-18 16:12:25 +0200

    (#505) Add a pop-up for subscription for V1
    - Fixes `cannot close` issue
    - Splits `full name` and passes its parts to Mailchimp form
    - Closes popup after form submit
    - Popup appears after 3 sec

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,19 +1,4 @@
 class WelcomeController < ApplicationController
-  # the react_component SubscriptionPopup is having issues with csrf_token and
-  # remove_session middleware, so we are temporarily disabling for this action
-  skip_before_filter :verify_authenticity_token, only: :subscription
-
   def index
-  end
-
-  def subscription
-    cookies.delete(:unbounded_subscription) unless Subscription.create(subscription_params)
-    redirect_to root_path
-  end
-
-  private
-
-  def subscription_params
-    params.require(:subscription).permit(:name, :email)
   end
 end
