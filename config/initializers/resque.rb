d0f58ff3d5874aa64ad091db8548aa21d5b6056d
Alexander Kuznetsov <paranoic.san@gmail.com>
2017-08-25 14:03:23 +0200

    (#530) Setup Airbrake for your Ruby application

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
+require 'airbrake/resque/failure'
+
 Resque.redis = ENV.fetch('REDIS_URL', 'redis://localhost:6379')
 Resque.redis.namespace = ENV.fetch('RESQUE_NAMESPACE', 'resque:development')
+
+project_id = ENV['AIR_BRAKE_PROJECT_ID']
+project_key = ENV['AIR_BRAKE_PROJECT_KEY']
+
+Resque::Failure.backend = Resque::Failure::Airbrake if project_id.present? && project_key.present?
