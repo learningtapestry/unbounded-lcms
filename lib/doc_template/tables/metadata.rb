d91ac6cd7989d4fba1078d7bb06e93d6bde53a22
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2017-09-14 09:57:54 -0300

    (#466) Ensure metadata table subject is always downcased

--- lib/doc_template/tables/metadata.rb
+++ lib/doc_template/tables/metadata.rb
@@ -1,9 +1,16 @@
+# frozen_string_literal: true
+
 module DocTemplate
   module Tables
     class Metadata < Base
-      HEADER_LABEL = 'document-metadata'.freeze
+      HEADER_LABEL = 'document-metadata'
       HTML_VALUE_FIELDS = %w(description materials preparation lesson-objective relationship-to-eny1).freeze
+
+      def parse(fragment)
+        super
+        @data['subject'] = @data['subject'].to_s.downcase if @data.present?
+        self
+      end
     end
   end
 end
-
