5383b4175d958e8b7865574e2354120a7211a0b8
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2017-10-11 11:42:02 -0300

    (#623) Dont show materials header if data is empty

--- app/services/react_materials_resolver.rb
+++ app/services/react_materials_resolver.rb
@@ -12,6 +12,8 @@ class ReactMaterialsResolver
 
     private
 
+    # rubocop:disable Metrics/CyclomaticComplexity
+    # rubocop:disable Metrics/PerceivedComplexity
     def replace_react(node, document)
       node.remove && return if (data = node.attr('data-react-props')).blank?
       raw_props = if data =~ /^\d+(?:,\s*\d+)*$/
@@ -23,6 +25,8 @@ class ReactMaterialsResolver
       node.remove && return if (raw_props['material_ids']).empty?
 
       props = PreviewsMaterialSerializer.new(raw_props, document)
+      node.remove && return if props.data&.empty?
+
       # TODO: find out what is wrong with server rendering here for web version
       component = h.react_component('MaterialsContainer', props, prerender: document.content_type != 'none')
       node.replace(component)
