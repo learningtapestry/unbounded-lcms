8f8a43eb4336bf4592dd9054f8c40cb192deb02d
Alexander Kuznetsov <paranoic.san@gmail.com>
2017-10-12 15:31:43 +0200

    (#612) [ELA] Activity Choice
    - Parses `optional` fields from metadata
    - Adds `Optional: ` prefix for optional section title

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,12 +1,16 @@
+# frozen_string_literal: true
+
 require 'database_cleaner'
 
 RSpec.configure do |config|
-  config.before(:each) do
+  config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.after(:each) do
-    DatabaseCleaner.clean
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
   end
 end
