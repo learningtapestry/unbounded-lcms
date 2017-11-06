26344b3cc3db459d5c65ae2c1efee8eaf5a8da6e
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2017-11-06 11:47:12 -0200

    (#715) Add search for external pages on V1

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 20170821064651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
+  enable_extension "pg_trgm"
 
   create_table "content_guide_definitions", force: :cascade do |t|
     t.string   "keyword",     null: false
