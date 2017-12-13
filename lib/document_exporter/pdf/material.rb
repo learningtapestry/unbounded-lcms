1585f5df541c26eaf4480b97e7b74e05789ff7bb
Alexander Kuznetsov <paranoic.san@gmail.com>
2017-12-13 17:48:43 +0200

    (#774) Remove duplex pages

--- lib/document_exporter/pdf/material.rb
+++ lib/document_exporter/pdf/material.rb
@@ -3,14 +3,6 @@
 module DocumentExporter
   module PDF
     class Material < PDF::Base
-      def export
-        content = super
-        pdf = CombinePDF.parse(content)
-        return content if pdf.pages.size.even?
-        @options[:blank_page] = true
-        super
-      end
-
       private
 
       def template_path(name)
