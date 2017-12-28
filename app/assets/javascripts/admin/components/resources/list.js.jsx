2a8c01b46b384921b5455d796b82b00c8afb1226
Alexander Kuznetsov <paranoic.san@gmail.com>
2017-12-28 11:29:58 +0200

    Fixes `with materials` behavior when reimporting single lesson
    Both reimport cases `Lesson index page` and `Lesson view page` were affected

--- app/assets/javascripts/admin/components/resources/list.js.jsx
+++ app/assets/javascripts/admin/components/resources/list.js.jsx
@@ -4,8 +4,8 @@
     if (!page.length) return;
 
     page.find('.c-reimport-with-materials__toggle input[type=checkbox]').change(function() {
-      const checked = $(this).prop('checked');
-      page.find('.c-reimport-doc-form .c-reimport-with-materials__field').val(checked);
+      const value = $(this).prop('checked') ? 1 : 0;
+      page.find('.c-reimport-doc-form .c-reimport-with-materials__field').val(value);
     });
   };
 })();
