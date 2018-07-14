8222692dfc109359384ca859ec7796bb2c2bebb7
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2018-07-13 23:28:23 -0300

    Fix broken migrations

--- db/migrate/20180307172827_remove_resource_backup.rb
+++ db/migrate/20180307172827_remove_resource_backup.rb
@@ -1,10 +1,18 @@
 class RemoveResourceBackup < ActiveRecord::Migration
-  def change
-    drop_table :resource_backups do |t|
+  def up
+    drop_table :resource_backups if conn.table_exists?('resource_backups')
+  end
+
+  def down
+    create_table :resource_backups do |t|
       t.string :comment, null: false
       t.string :dump
 
       t.timestamps null: false
     end
+  end
+
+  def conn
+    ActiveRecord::Base.connection
   end
 end
