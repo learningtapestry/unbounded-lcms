e7138ad404bcc2608d98ec53612f18c25bc6b5b3
Abraham Sánchez <abraham@twintags.com>
2018-11-01 13:35:08 +0100

    Remove/Decorate entities provided by the engine

---
+++ app/decorators/entities/filterbar_decorator.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+Filterbar.class_eval do
+  FACETS = %w(grade module unit lesson content_guide multimedia other).freeze
+end
--- app/models/reading_assignment_author.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class ReadingAssignmentAuthor < ActiveRecord::Base
-  has_many :reading_assignment_texts
-end
